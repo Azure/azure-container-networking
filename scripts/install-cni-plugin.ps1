fee0ec79fc11da4e3e67b3d4b593f413218b7797
Onur Filiz <onur.filiz@microsoft.com>
2017-04-12 15:37:39 -0700

    Added install-cni-plugin scripts

--- scripts/install-cni-plugin.ps1
+++ scripts/install-cni-plugin.ps1
@@ -8,34 +8,23 @@
 [CmdletBinding(DefaultParameterSetName="Standard")]
 param(
     [string]
+    [parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     $PluginVersion = "v0.7",
 
+    [string]
     [parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
     $CniBinDir = "c:\cni\bin",
 
+    [string]
     [parameter(Mandatory=$false)]
     [ValidateNotNullOrEmpty()]
-    $CniConfDir = "c:\cni\netconf"
+    $CniNetConfDir = "c:\cni\netconf"
 )
 
 function
-Write-Log($message)
-{
-    Write-Host $message
-}
-
-function
-Download-File($uri, $destination)
-{
-    $temp = "c:\k.zip"
-    Invoke-WebRequest -Uri $uri -OutFile $temp
-    Extract-ZipFile -File $temp -Destination $destination
-}
-
-function
-Extract-ZipFile($file, $destination)
+Expand-File($file, $destination)
 {
     $shell = new-object -com shell.application
     $zip = $shell.NameSpace($file)
@@ -47,18 +36,25 @@ Extract-ZipFile($file, $destination)
 
 try {
     # Create CNI directories.
-    mkdir $CniBinDir
-    mkdir $CniConfDir
+    Write-Host "Creating CNI directories."
+    New-Item $CniBinDir -Type directory -Force > $null
+    New-Item $CniNetConfDir -Type directory -Force > $null
 
-    # Install Azure CNI plugins.
-    Download-File https://github.com/Azure/azure-container-networking/releases/download/$PluginVersion/azure-vnet-cni-windows-amd64-$PluginVersion.zip > $CniBinDir/azure.zip
-    Extract-ZipFile $CniBinDir/azure.zip $CniBinDir
+    # Install azure-vnet CNI plugins.
+    Write-Host "Installing azure-vnet CNI plugin version $PluginVersion to $CniBinDir..."
+    Invoke-WebRequest -Uri https://github.com/Azure/azure-container-networking/releases/download/$PluginVersion/azure-vnet-cni-windows-amd64-$PluginVersion.zip -OutFile $CniBinDir\azure-vnet.zip
+    Expand-File $CniBinDir\azure-vnet.zip $CniBinDir
+
+    # Install azure-vnet CNI network configuration file.
+    Write-Host "Installing azure-vnet CNI network configuration file to $CniNetConfDir..."
+    Move-Item $CniBinDir\*.conf $CniNetConfDir -Force
 
     # Windows does not need a loopback plugin.
 
     # Cleanup.
-    del $CniBinDir/*.zip
-    #chown root:root $CNI_BIN_DIR/*
+    Remove-Item $CniBinDir\azure-vnet.zip
+
+    Write-Host "azure-vnet CNI plugin is successfully installed."
 }
 catch
 {
