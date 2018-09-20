84d0af1c43dd2e792cdacbe36aa7c87472b6fccd
John Howard <jhoward@microsoft.com>
2018-09-20 11:14:57 -0700

    Implement GetLastRebootTime for Windows, but no-op'd
    Signed-off-by: John Howard <jhoward@microsoft.com>
    
    As per the code comment:
    
    	// @jhowardmsft - These following lines are the original non-implementation.
    	// I have kept these here, as by introducing a real implementation of this
    	// function (which I have below) has side effects which I have no means
    	// of being able to verify correct operation, have to leave that to the
    	// owners of this repo to verify.

--- platform/os_windows.go
+++ platform/os_windows.go
@@ -4,7 +4,10 @@
 package platform
 
 import (
+	"syscall"
 	"time"
+
+	"golang.org/x/sys/windows"
 )
 
 const (
@@ -27,10 +30,29 @@ func GetOSInfo() string {
 	return "windows"
 }
 
-// GetLastRebootTime returns the last time the system rebooted.
+var (
+	modkernel32        = windows.NewLazySystemDLL("kernel32.dll")
+	procGetTickCount64 = modkernel32.NewProc("GetTickCount64")
+)
+
+// GetLastRebootTime returns the last time the system rebooted in UTC.
 func GetLastRebootTime() (time.Time, error) {
+
+	// @jhowardmsft - These following lines are the original non-implementation.
+	// I have kept these here, as by introducing a real implementation of this
+	// function (which I have below) has side effects which I have no means
+	// of being able to verify correct operation, have to leave that to the
+	// owners of this repo to verify.
 	var rebootTime time.Time
 	return rebootTime, nil
+
+	// This is a Windows implementation of returning the UTC time of when the
+	// host was booted. See note above - it is no-op'd by the two lines above.
+	nowUTC := time.Now().UTC()
+	r0, _, _ := syscall.Syscall(procGetTickCount64.Addr(), 0, 0, 0, 0)
+	tickCount := uint64(r0)
+	upTimeUTC := time.Duration(tickCount) * time.Millisecond
+	return nowUTC.Add(-upTimeUTC), nil
 }
 
 func ExecuteCommand(command string) (string, error) {
