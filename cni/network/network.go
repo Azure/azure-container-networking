d6e5d3db34dd22e53dd83ae6e5cd7b8ec42bf7f8
Yongli Chen <12708785@qq.com>
2018-10-05 22:06:56 -0700

    remove network dns suffix and preserve endpoint dns suffix

--- cni/network/network.go
+++ cni/network/network.go
@@ -6,7 +6,6 @@ package network
 import (
 	"fmt"
 	"net"
-	"strings"
 
 	"github.com/Azure/azure-container-networking/cni"
 	"github.com/Azure/azure-container-networking/cns"
@@ -327,6 +326,12 @@ func (plugin *netPlugin) Add(args *cniSkel.CmdArgs) error {
 			return err
 		}
 
+		nwDnsInfo, err := getNetworkDNSSettings(nwCfg, result, k8sNamespace)
+		if err != nil {
+			err = plugin.Errorf("Failed to getDNSSettings: %v", err)
+			return err
+		}
+
 		// Create the network.
 		nwInfo := network.NetworkInfo{
 			Id:   networkId,
@@ -340,11 +345,8 @@ func (plugin *netPlugin) Add(args *cniSkel.CmdArgs) error {
 			},
 			BridgeName:       nwCfg.Bridge,
 			EnableSnatOnHost: nwCfg.EnableSnatOnHost,
-			DNS: network.DNSInfo{
-				Servers: nwCfg.DNS.Nameservers,
-				Suffix:  k8sNamespace + "." + strings.Join(nwCfg.DNS.Search, ","),
-			},
-			Policies: policies,
+			DNS:              nwDnsInfo,
+			Policies:         policies,
 		}
 
 		nwInfo.Options = make(map[string]interface{})
@@ -386,6 +388,12 @@ func (plugin *netPlugin) Add(args *cniSkel.CmdArgs) error {
 		}
 	}
 
+	epDnsInfo, err := getEndpointDNSSettings(nwCfg, result, k8sNamespace)
+	if err != nil {
+		err = plugin.Errorf("Failed to getEndpointDNSSettings: %v", err)
+		return err
+	}
+
 	epInfo = &network.EndpointInfo{
 		Id:               endpointId,
 		ContainerID:      args.ContainerID,
@@ -393,17 +401,10 @@ func (plugin *netPlugin) Add(args *cniSkel.CmdArgs) error {
 		IfName:           args.IfName,
 		EnableSnatOnHost: nwCfg.EnableSnatOnHost,
 		EnableInfraVnet:  enableInfraVnet,
+		Data:             make(map[string]interface{}),
+		DNS:              epDnsInfo,
+		Policies:         policies,
 	}
-	epInfo.Data = make(map[string]interface{})
-
-	dns, err := getDNSSettings(nwCfg, result, k8sNamespace)
-	if err != nil {
-		log.Printf("Error retrieving dns settings %v", err)
-		return err
-	}
-
-	epInfo.DNS = dns
-	epInfo.Policies = policies
 
 	// Populate addresses.
 	for _, ipconfig := range result.IPs {
