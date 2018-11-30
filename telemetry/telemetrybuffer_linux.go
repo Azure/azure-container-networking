231a17af33e783d2a49b0eeceedce7b37e02d58e
Jaeryn Chu <Tsun.Chu@microsoft.com>
2018-11-30 15:23:44 -0800

    Adding telemetry buffer.

---
+++ telemetry/telemetrybuffer_linux.go
@@ -0,0 +1,33 @@
+// Copyright 2017 Microsoft. All rights reserved.
+// MIT License
+
+package telemetry
+
+import (
+	"fmt"
+	"net"
+)
+
+const (
+	fdTemplate = "/tmp/%s.sock"
+)
+
+// Dial - try to connect to/create a socket with 'name'
+func (tb *TelemetryBuffer) Dial(name string) (err error) {
+	conn, err := net.Dial("unix", fmt.Sprintf(fdTemplate, name))
+	if err == nil {
+		tb.client = conn
+	}
+
+	return err
+}
+
+// Listen - try to create and listen on socket with 'name'
+func (tb *TelemetryBuffer) Listen(name string) (err error) {
+	conn, err := net.Listen("unix", fmt.Sprintf(fdTemplate, name))
+	if err == nil {
+		tb.listener = conn
+	}
+
+	return err
+}
