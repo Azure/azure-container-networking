f2dca07281e0a1d2ed0337700b8fa4bdf76ae603
Jaeryn Chu <Tsun.Chu@microsoft.com>
2019-07-09 09:42:34 -0700

    Ignoring unmarshal errors when invoking CNI.

--- cns/networkcontainers/networkcontainers.go
+++ cns/networkcontainers/networkcontainers.go
@@ -153,7 +153,7 @@ func pluginErr(err error, output []byte) error {
 		}
 	} else if len(output) > 0 {
 		var cniError cniTypes.Error
-		if err = json.Unmarshal(output, &cniError); err == nil && cniError.Code != 0 {
+		if unmarshalErr := json.Unmarshal(output, &cniError); unmarshalErr == nil && cniError.Code != 0 {
 			return fmt.Errorf("netplugin completed with error: %+v", cniError)
 		}
 	}
