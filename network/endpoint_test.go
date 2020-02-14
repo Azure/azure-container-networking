3cdf9d0a631d93986779194f70417fb46e280270
QianYu <348368689@qq.com>
2020-02-14 15:25:57 +0800

    rename manager_test && add endpoint_test

--- network/endpoint_test.go
+++ network/endpoint_test.go
@@ -4,21 +4,36 @@
 package network
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"testing"
 )
 
-func TestGetPodName(t *testing.T) {
-	testData := map[string]string{
-		"nginx-deployment-5c689d88bb":       "nginx",
-		"nginx-deployment-5c689d88bb-qwq47": "nginx-deployment",
-		"nginx": "nginx",
-	}
 
-	for testValue, expectedPodName := range testData {
-		podName := GetPodNameWithoutSuffix(testValue)
-
-		if podName != expectedPodName {
-			t.Error("Expected:", expectedPodName, ", Got: ", podName, ", For Test Value:", testValue)
-		}
-	}
+func TestEndpoint(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Network Suite")
 }
+
+var (
+	_ = Describe("Test Endpoint", func() {
+
+		Describe("Test GetPodName", func() {
+			Context("GetPodName", func() {
+				It("GetPodName successfully", func() {
+
+					testData := map[string]string{
+						"nginx-deployment-5c689d88bb":       "nginx",
+						"nginx-deployment-5c689d88bb-qwq47": "nginx-deployment",
+						"nginx": "nginx",
+					}
+
+					for testValue, expectedPodName := range testData {
+						podName := GetPodNameWithoutSuffix(testValue)
+						Expect(podName).To(Equal(expectedPodName))
+					}
+				})
+			})
+		})
+	})
+)
