5489d59bf14a02adaf8873e101fbecb57f8bc09e
QianYu <348368689@qq.com>
2020-02-21 16:36:52 +0800

    add the origin manager test case

--- ipam/manager_test.go
+++ ipam/manager_test.go
@@ -420,5 +420,176 @@ var (
 				})
 			})
 		})
+
+		// Tests updating an existing address space adds new resources and removes stale ones.
+		Describe("Test Address Space Update", func() {
+			Context("When update the Address Space", func() {
+				It("Should update successfully", func() {
+					// Start with the test address space.
+					options := map[string]interface{}{}
+					am, err := createAddressManager(options)
+					Expect(err).ToNot(HaveOccurred())
+		
+					amImpl := am.(*addressManager)
+		
+					// Create a new local address space to update the existing one.
+					localAs, err := amImpl.newAddressSpace(LocalDefaultAddressSpaceId, LocalScope)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Remove addr12 and add addr13 in subnet1.
+					ap, err := localAs.newAddressPool(anyInterface, anyPriority, &subnet1)
+					Expect(err).ToNot(HaveOccurred())
+					_, err = ap.newAddressRecord(&addr11)
+					Expect(err).ToNot(HaveOccurred())
+					_, err = ap.newAddressRecord(&addr13)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Remove subnet2.
+					// Add subnet3 with addr31.
+					ap, err = localAs.newAddressPool(anyInterface, anyPriority, &subnet3)
+					Expect(err).ToNot(HaveOccurred())
+					_, err = ap.newAddressRecord(&addr31)
+					Expect(err).ToNot(HaveOccurred())
+		
+					err = amImpl.setAddressSpace(localAs)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Test that the address space was updated correctly.
+					localAs, err = amImpl.getAddressSpace(LocalDefaultAddressSpaceId)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Subnet1 should have addr11 and addr13, but not addr12.
+					ap, err = localAs.getAddressPool(subnet1.String())
+					Expect(err).ToNot(HaveOccurred())
+		
+					_, err = ap.requestAddress(addr11.String(), nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					_, err = ap.requestAddress(addr12.String(), nil)
+					Expect(err).To(HaveOccurred())
+		
+					_, err = ap.requestAddress(addr13.String(), nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Subnet2 should not exist.
+					ap, err = localAs.getAddressPool(subnet2.String())
+					Expect(err).To(HaveOccurred())
+		
+					// Subnet3 should have addr31 only.
+					ap, err = localAs.getAddressPool(subnet3.String())
+					Expect(err).ToNot(HaveOccurred())
+		
+					_, err = ap.requestAddress(addr31.String(), nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					_, err = ap.requestAddress(addr32.String(), nil)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+		})
+		
+		Describe("Test Address Pool requests", func() {
+			// Tests multiple wildcard address pool requests return separate pools.
+			Context("When request pool for separate pools", func() {
+				It("Should request and release pool successfully", func() {
+					// Start with the test address space.
+					options := map[string]interface{}{}
+					am, err := createAddressManager(options)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Request two separate address pools.
+					poolId1, subnet1, err := am.RequestPool(LocalDefaultAddressSpaceId, "", "", nil, false)
+					Expect(err).ToNot(HaveOccurred())
+		
+					poolId2, subnet2, err := am.RequestPool(LocalDefaultAddressSpaceId, "", "", nil, false)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Test the poolIds and subnets do not match.
+					Expect(poolId1).ToNot(Equal(poolId2))
+					Expect(subnet1).ToNot(Equal(subnet2))
+		
+					// Release the address pools.
+					err = am.ReleasePool(LocalDefaultAddressSpaceId, poolId1)
+					Expect(err).ToNot(HaveOccurred())
+		
+					err = am.ReleasePool(LocalDefaultAddressSpaceId, poolId2)
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
+		
+			// Tests multiple identical address pool requests return the same pool and pools are referenced correctly.
+			Context("When request pool for same pool", func() {
+				It("Should request and release pool successfully", func() {
+					// Start with the test address space.
+					options := map[string]interface{}{}
+					am, err := createAddressManager(options)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Request the same address pool twice.
+					poolId1, subnet1, err := am.RequestPool(LocalDefaultAddressSpaceId, "", "", nil, false)
+					Expect(err).ToNot(HaveOccurred())
+		
+					poolId2, subnet2, err := am.RequestPool(LocalDefaultAddressSpaceId, poolId1, "", nil, false)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Test the subnets do not match.
+					Expect(poolId1).To(Equal(poolId2))
+					Expect(subnet1).To(Equal(subnet2))
+		
+					// Release the address pools.
+					err = am.ReleasePool(LocalDefaultAddressSpaceId, poolId1)
+					Expect(err).ToNot(HaveOccurred())
+		
+					err = am.ReleasePool(LocalDefaultAddressSpaceId, poolId2)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Third release should fail.
+					err = am.ReleasePool(LocalDefaultAddressSpaceId, poolId1)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+		})
+		
+		Describe("Test Address requests", func() {
+			// Tests address requests from the same pool return separate addresses and releases work correctly.
+			Context("When request from the same pool", func() {
+				It("Should request and release successfully", func() {
+					// Start with the test address space.
+					options := map[string]interface{}{}
+					am, err := createAddressManager(options)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Request a pool.
+					poolId, _, err := am.RequestPool(LocalDefaultAddressSpaceId, "", "", nil, false)
+					Expect(err).ToNot(HaveOccurred())
+		
+					// Request two addresses from the pool.
+					address1, err := am.RequestAddress(LocalDefaultAddressSpaceId, poolId, "", nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					addr, _, _ := net.ParseCIDR(address1)
+					address1 = addr.String()
+		
+					address2, err := am.RequestAddress(LocalDefaultAddressSpaceId, poolId, "", nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					addr, _, _ = net.ParseCIDR(address2)
+					address2 = addr.String()
+		
+					// Test the addresses do not match.
+					Expect(address1).ToNot(Equal(address2))
+		
+					// Release addresses and the pool.
+					err = am.ReleaseAddress(LocalDefaultAddressSpaceId, poolId, address1, nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					err = am.ReleaseAddress(LocalDefaultAddressSpaceId, poolId, address2, nil)
+					Expect(err).ToNot(HaveOccurred())
+		
+					err = am.ReleasePool(LocalDefaultAddressSpaceId, poolId)
+					Expect(err).ToNot(HaveOccurred())
+				})
+			})
+		})
 	})
 )
