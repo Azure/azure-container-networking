c24f204496d0a70f57661bed58b4004c03389f4d
Vivek Aggarwal <vivekagg@microsoft.com>
2020-10-14 11:11:39 -0700

    Addressing Raimro's feedback

--- cni/utils/errutils.go
+++ cni/utils/errutils.go
@@ -1,20 +1,15 @@
 package utils
 
 import (
-	"errors"
-
 	"github.com/Azure/azure-container-networking/cns/cnsclient"
 	"github.com/Azure/azure-container-networking/cns/restserver"
 )
 
 // TODO : Move to common directory like common, after fixing circular dependencies
 func IsNotFoundError(err error) bool {
-	switch err := err.(type) {
+	switch e := err.(type) {
 	case *cnsclient.CNSClientError:
-		var cnsError *cnsclient.CNSClientError
-		// not expected to fail
-		_ = errors.As(err, &cnsError)
-		return (cnsError.Code == restserver.UnknownContainerID)
+		return (e.Code == restserver.UnknownContainerID)
 	}
 	return false
 }
