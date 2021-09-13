921e439b516dd41c577a99751ceccb8e5aac13de
Paul Johnston <johnstonpaul801@gmail.com>
2021-09-13 09:57:52 -0700

    First blood...

--- cns/cnsclient/error_test.go
+++ cns/cnsclient/error_test.go
@@ -1,6 +1,7 @@
 package cnsclient
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/Azure/azure-container-networking/cns/types"
@@ -40,6 +41,31 @@ func TestIsNotFound(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IsNotFound(tt.err); got != tt.want {
 				t.Errorf("IsNotFound() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestError(t *testing.T) {
+	tests := []struct {
+		name string
+		err  error
+		want string
+	}{
+		{
+			name: "is not found",
+			err: &CNSClientError{
+				Code: types.UnknownContainerID,
+				Err:  errors.New("unknown container id"),
+			},
+			want: fmt.Sprintf("[Azure CNSClient] Code: %d , Error: %v", types.UnknownContainerID, errors.New("unknown container id")),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.err.Error(); got != tt.want {
+				t.Errorf("Error() = %v, want %v", got, tt.want)
 			}
 		})
 	}
