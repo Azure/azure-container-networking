801de93de5df51f7e6e0aa3b4b58e894ce39f5fc
Paul Johnston <johnstonpaul801@gmail.com>
2021-09-21 10:23:03 -0700

    feedback: making CNSRESTError struct

--- cns/restserver/errors.go
+++ cns/restserver/errors.go
@@ -1,18 +1,27 @@
 package restserver
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/Azure/azure-container-networking/cns/types"
 )
 
-var errResponseCode = errors.New("Response code is error")
+// CNSRESTError represents a CNS error
+type CNSRESTError struct {
+	ResponseCode types.ResponseCode
+}
+
+func (c *CNSRESTError) Error() string {
+	return fmt.Sprintf("response code: %s", c.ResponseCode.String())
+}
 
 // ResponseCodeToError converts a cns response code to error type. If the response code is OK, then return value is nil
 func ResponseCodeToError(responseCode types.ResponseCode) error {
-	if responseCode == 0 {
+	if responseCode == types.Success {
 		return nil
 	}
-	return fmt.Errorf("%w: %v", errResponseCode, responseCode)
+
+	return &CNSRESTError{
+		ResponseCode: responseCode,
+	}
 }
