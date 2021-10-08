c07190e6443366c7a2b31b9566ed90335ef77a0d
Vamsi Kalapala <vkalapa@ncsu.edu>
2021-10-08 10:39:22 -0700

    splitting ioshim for each os

---
+++ common/ioshim_linux.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"github.com/Azure/azure-container-networking/network/hnswrapper"
+	testutils "github.com/Azure/azure-container-networking/test/utils"
+	utilexec "k8s.io/utils/exec"
+)
+
+type IOShim struct {
+	Exec utilexec.Interface
+}
+
+func NewIOShim() *IOShim {
+	return &IOShim{
+		Exec: utilexec.New(),
+	}
+}
+
+func NewMockIOShim(calls []testutils.TestCmd) *IOShim {
+	return &IOShim{
+		Exec: testutils.GetFakeExecWithScripts(calls),
+	}
+}
