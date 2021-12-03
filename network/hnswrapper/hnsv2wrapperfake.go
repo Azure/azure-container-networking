9f8623f86998c87e114687e0b1062dc531f4f074
Vamsi Kalapala <vakr@microsoft.com>
2021-12-03 13:00:09 -0800

    Fixing hnsfake

--- network/hnswrapper/hnsv2wrapperfake.go
+++ network/hnswrapper/hnsv2wrapperfake.go
@@ -73,7 +73,7 @@ func (f Hnsv2wrapperFake) ModifyNetworkSettings(network *hcn.HostComputeNetwork,
 				if err != nil {
 					return newErrorFakeHNS(err.Error())
 				}
-				if setpol.Type != hcn.SetPolicyTypeIpSet {
+				if setpol.PolicyType != hcn.SetPolicyTypeIpSet {
 					// Check Nested SetPolicy members
 					members := strings.Split(setpol.Values, ",")
 					for _, memberID := range members {
@@ -101,10 +101,10 @@ func (f Hnsv2wrapperFake) ModifyNetworkSettings(network *hcn.HostComputeNetwork,
 			if _, ok := networkCache.Policies[setpol.Id]; !ok {
 				return newErrorFakeHNS(fmt.Sprintf("[FakeHNS] could not find %s ipset", setpol.Name))
 			}
-			if setpol.Type == hcn.SetPolicyTypeIpSet {
+			if setpol.PolicyType == hcn.SetPolicyTypeIpSet {
 				// For 1st level sets check if they are being referred by nested sets
 				for _, cacheSet := range networkCache.Policies {
-					if cacheSet.Type == hcn.SetPolicyTypeIpSet {
+					if cacheSet.PolicyType == hcn.SetPolicyTypeIpSet {
 						continue
 					}
 					if strings.Contains(cacheSet.Values, setpol.Id) {
@@ -134,7 +134,7 @@ func (f Hnsv2wrapperFake) ModifyNetworkSettings(network *hcn.HostComputeNetwork,
 				// Replicating HNS behavior, we will not update non-existent set policy
 				continue
 			}
-			if setpol.Type != hcn.SetPolicyTypeIpSet {
+			if setpol.PolicyType != hcn.SetPolicyTypeIpSet {
 				// Check Nested SetPolicy members
 				members := strings.Split(setpol.Values, ",")
 				for _, memberID := range members {
