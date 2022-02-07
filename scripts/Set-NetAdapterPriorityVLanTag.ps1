5c019cf0983390b9ac537d6547426bc6fb3ca371
Paul Johnston <paujohns@microsoft.com>
2022-02-07 12:40:17 -0800

    Adapt to x-3 version

--- scripts/Set-NetAdapterPriorityVLanTag.ps1
+++ scripts/Set-NetAdapterPriorityVLanTag.ps1
@@ -1,24 +1,49 @@
 function Set-NetAdapterPriorityVLanTag
 {
-    Write-Host "Searching for a network adapter with 'Mellanox' in description"
-    $ethernetName = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like "*Mellanox*" } | Select-Object -ExpandProperty Name
+    New-Variable -Name PriorityVLANTagIdentifier -Value "*PriorityVLANTag" -Option ReadOnly
+    New-Variable -Name MellanoxSearchString -Value "*Mellanox*" -Option ReadOnly
+    New-Variable -Name RegistryKeyPrefix -Value "HKLM:\System\CurrentControlSet\Control\Class\" -Option ReadOnly
+
+    Write-Host "Searching for a network adapter with '$MellanoxSearchString' in description"
+    $ethernetName = Get-NetAdapter | Where-Object { $_.InterfaceDescription -like $MellanoxSearchString } | Select-Object -ExpandProperty Name
 
     if ($ethernetName)
     {
         Write-Host "Network adapter found: '$ethernetName'"
-        $ethernetNameIfInProperty = Get-NetAdapterAdvancedProperty | Where-Object { $_.RegistryKeyword -like "*PriorityVLANTag" -and $_.Name -eq $ethernetName } | Select-Object -ExpandProperty Name
+        $ethernetNameIfInProperty = Get-NetAdapterAdvancedProperty | Where-Object { $_.RegistryKeyword -like $PriorityVLANTagIdentifier -and $_.Name -eq $ethernetName } | Select-Object -ExpandProperty Name
 
-        Write-Host "Searching network adapter properties for '*PriorityVLANTag'"
+        Write-Host "Searching network adapter properties for '$PriorityVLANTagIdentifier'"
         if ($ethernetNameIfInProperty)
         {
-            Write-Host "Found 'PriorityVLANTag' in adapter's advanced properties"
-            Set-NetAdapterAdvancedProperty -Name $ethernetName -RegistryKeyword "*PriorityVLANTag" -RegistryValue 3
-            Write-Host "Successfully set Mellanox Network Adapter: '$ethernetName' with '*PriorityVLANTag' property value as 3"
+            Write-Host "Found '$PriorityVLANTagIdentifier' in adapter's advanced properties"
+            Set-NetAdapterAdvancedProperty -Name $ethernetName -RegistryKeyword $PriorityVLANTagIdentifier -RegistryValue 3
+            Write-Host "Successfully set Mellanox Network Adapter: '$ethernetName' with '$PriorityVLANTagIdentifier' property value as 3"
             return;
         }
 
-        Write-Host "Could not find 'PriorityVLANTag' in adapter's advanced properties"
-        Write-Host "Proceeding to set in a different way"
+        Write-Host "Could not find '$PriorityVLANTagIdentifier' in adapter's advanced properties"
 
+        Write-Host "Proceeding in a different way"
+        Write-Host "Searching through CIM instances for Network devices with '$MellanoxSearchString' in the name"
+        $deviceID = Get-CimInstance -Namespace root/cimv2 -ClassName Win32_PNPEntity | Where-Object PNPClass -EQ "Net" | Where-Object { $_.Name -like $MellanoxSearchString } | Select-Object -ExpandProperty DeviceID
+        if ($deviceID)
+        {
+            Write-Host "Device ID found: '$deviceID'"
+            Write-Host "Getting Pnp Device properites for '$deviceID'"
+            $registryKeySuffix = Get-PnpDeviceProperty -InstanceId $deviceID | Where-Object KeyName -EQ "DEVPKEY_Device_Driver" | Select-Object -ExpandProperty Data
+            Write-Host "Registry key suffix found: '$registryKeySuffix'"
+            $registryKeyFullPath = $RegistryKeyPrefix + $registryKeySuffix
+            Write-Host "Registry key full path: '$registryKeyFullPath'"
+            Write-Host "Updating '$PriorityVLANTagIdentifier' to be 3"
+            New-ItemProperty -Path $registryKeyFullPath -Name $PriorityVLANTagIdentifier -Value 3 -PropertyType String -Force
+            Write-Host "Updated successfully"
+            return;
+        }
+
+        Write-Host "No network device found with '$MellanoxSearchString' in name, exiting."
+        return;
     }
+
+    Write-Host "No Network adapter found with '$MellanoxSearchString' in description"
+    return;
 }
