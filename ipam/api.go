34deaa7fce25ef1336c0cb84cdd959a6cc54eacd
Keith Nguyen <keithnguyen@microsoft.com>
2022-06-01 15:50:54 -0500

    style: lint issues

--- ipam/api.go
+++ ipam/api.go
@@ -10,18 +10,15 @@ import (
 var (
 	// Error responses returned by AddressManager.
 	errInvalidAddressSpace  = fmt.Errorf("Invalid address space")
-	errInvalidPoolId        = fmt.Errorf("Invalid address pool")
+	errInvalidPoolID        = fmt.Errorf("Invalid address pool")
 	errInvalidAddress       = fmt.Errorf("Invalid address")
 	errInvalidScope         = fmt.Errorf("Invalid scope")
 	errInvalidConfiguration = fmt.Errorf("Invalid configuration")
 	errAddressPoolExists    = fmt.Errorf("Address pool already exists")
 	errAddressPoolNotFound  = fmt.Errorf("Address pool not found")
-	errAddressPoolInUse     = fmt.Errorf("Address pool already in use")
-	errAddressPoolNotInUse  = fmt.Errorf("Address pool not in use")
 	errAddressExists        = fmt.Errorf("Address already exists")
 	errAddressNotFound      = fmt.Errorf("Address not found")
 	errAddressInUse         = fmt.Errorf("Address already in use")
-	errAddressNotInUse      = fmt.Errorf("Address not in use")
 	errNoAvailableAddresses = fmt.Errorf("No available addresses")
 
 	// Exportable errors returned by AddressManager
