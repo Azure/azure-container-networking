01ab1412d03555c6a45d20257215ddb1226b89ac
Cristina Kovacs <ckovacs@microsoft.com>
2022-09-19 14:59:10 -0500

    added newline and kubectl update

--- debug/windows/npm/win-debug.ps1
+++ debug/windows/npm/win-debug.ps1
@@ -1,5 +1,5 @@
 $filepath = "logs_$((Get-Date).ToString('MM-dd-yyyy'))"
-kubectl get pod -A >> ( New-Item -Path ./$filepath/allpods.out -Force )
+kubectl get pod -A -o wide >> ( New-Item -Path ./$filepath/allpods.out -Force )
 $npmpod = kubectl get pod -n kube-system -owide --output=custom-columns='Name:.metadata.name,Node:spec.nodeName' | Select-String "npm-win"
 $rows = @()
 foreach ($row in (-split $npmpod)) {
