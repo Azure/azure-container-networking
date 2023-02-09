6d911b9f8bae8bb101d150ac4fa4cb6719b788e4
paulyu <paulyu@microsoft.com>
2023-02-09 11:28:19 -0500

    fix installation script

--- cni/scripts/installcni.ps1
+++ cni/scripts/installcni.ps1
@@ -1,7 +1,7 @@
 Write-Host $env:CONTAINER_SANDBOX_MOUNT_POINT 
 
 $sourceCNI = $env:CONTAINER_SANDBOX_MOUNT_POINT + "\azure-container-networking\cni\network\plugin\azure-vnet.exe"
-$sourceConflist = $env:CONTAINER_SANDBOX_MOUNT_POINT + "\azure-container-networking\cni\azure-windows-swift.conflist"
+$sourceConflist = $env:CONTAINER_SANDBOX_MOUNT_POINT + "\azure-container-networking\cni\azure-windows-multitenancy.conflist"
 $sourceCNIVersion = & "$sourceCNI" -v
 $currentVersion = ""
 
@@ -17,10 +17,13 @@ Write-Host "Current Host $currentVersion"
 
 ## check CNI was already installed so not to get stuck in a infinite loop of rebooting
 if ($currentVersion -ne $sourceCNIVersion){
+    Write-Host "about copying azure-vnet to windows node..."
     Rename-Item -Path "C:\k\azurecni\bin\azure-vnet.exe" -NewName "azure-vnet-old.exe"
     Copy-Item $sourceCNI -Destination "C:\k\azurecni\bin"
-    Copy-Item $sourceConflist -Destination "C:\k\azurecni\netconf\10-azure.conflist"
-    Rename-Item -Path $sourceConflist -NewName "10-azure.conflist"
+    Write-Host "about copying 10-azure.conflist file to windows node..."
+    Remove-Item "C:\k\azurecni\netconf\10-azure.conflist"
+    Copy-Item $sourceConflist -Destination "C:\k\azurecni\netconf"
+    Rename-Item -Path "C:\k\azurecni\netconf\azure-windows-multitenancy.conflist" -NewName "10-azure.conflist"
 
     shutdown /r /t 0
 }
