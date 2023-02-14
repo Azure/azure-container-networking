3b4e45e9c0c0c72b3bf404052453d200800c7f9d
paulyu <paulyu@microsoft.com>
2023-02-13 22:56:13 -0500

    fix CNI containerized image

--- cni/scripts/installcni.ps1
+++ cni/scripts/installcni.ps1
@@ -18,12 +18,13 @@ Write-Host "Current Host $currentVersion"
 ## check CNI was already installed so not to get stuck in a infinite loop of rebooting
 if ($currentVersion -ne $sourceCNIVersion){
     Write-Host "about copying azure-vnet to windows node..."
-    Rename-Item -Path "C:\k\azurecni\bin\azure-vnet.exe" -NewName "azure-vnet-old.exe"
+    Remove-Item "C:\k\azurecni\bin\azure-vnet.exe"
     Copy-Item $sourceCNI -Destination "C:\k\azurecni\bin"
     Write-Host "about copying 10-azure.conflist file to windows node..."
     Remove-Item "C:\k\azurecni\netconf\10-azure.conflist"
     Copy-Item $sourceConflist -Destination "C:\k\azurecni\netconf"
     Rename-Item -Path "C:\k\azurecni\netconf\azure-windows-multitenancy.conflist" -NewName "10-azure.conflist"
-
-    shutdown /r /t 0
+    #shutdown /r /t 0
 }
+
+Start-Sleep -s 1000
