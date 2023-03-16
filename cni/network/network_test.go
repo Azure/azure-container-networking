bd8862f14789428096115d7fdcce985945d176ed
paulyu <paulyu@microsoft.com>
2023-03-16 12:42:08 -0400

    add a comment for setting up ipv6 flag

--- cni/network/network_test.go
+++ cni/network/network_test.go
@@ -73,7 +73,7 @@ func GetTestResources() *NetPlugin {
 	plugin.report = &telemetry.CNIReport{}
 	mockNetworkManager := acnnetwork.NewMockNetworkmanager()
 	plugin.nm = mockNetworkManager
-	plugin.ipamInvoker = NewMockIpamInvoker(true, false, false)
+	plugin.ipamInvoker = NewMockIpamInvoker(true, false, false) // enable ipv6 flag for TestAddDualStack test case
 	return plugin
 }
 
