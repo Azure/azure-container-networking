e2a87f197a0388cb0d0917282e7323392cf2f1df
paulyu <paulyu@microsoft.com>
2023-04-17 10:17:47 -0400

    remove nolint

--- network/network_windows.go
+++ network/network_windows.go
@@ -212,32 +212,32 @@ func (nm *networkManager) addNewNetRules(nwInfo *NetworkInfo) error {
 
 		ip, _, er := net.ParseCIDR(prefix)
 		if er != nil {
-			return fmt.Errorf("[net] failed to parse prefix %s", prefix) //nolint
+			return fmt.Errorf("[net] failed to parse prefix %s", prefix)
 		}
 		if ip.To4() != nil {
 			// netsh interface ipv4 add route $subnetV4 $hostInterfaceAlias "0.0.0.0"
 			netshV4DefaultRoute := fmt.Sprintf(netRouteCmd, "ipv4", prefix, ifName, ipv4DefaultHop)
 			if out, err = nm.plClient.ExecuteCommand(netshV4DefaultRoute); err != nil {
-				log.Printf("[net] Adding ipv4 default route failed: %v:%v", out, err) //nolint
+				log.Printf("[net] Adding ipv4 default route failed: %v:%v", out, err)
 			}
 
 			// netsh interface ipv4 add route $subnetV4 $hostInterfaceAlias $gatewayV4
 			netshV4GatewayRoute := fmt.Sprintf(netRouteCmd, "ipv4", prefix, ifName, gateway)
 			log.Printf("[net] Adding ipv4 gateway route failed: %v:%v", out, err)
 			if out, err = nm.plClient.ExecuteCommand(netshV4GatewayRoute); err != nil {
-				log.Printf("[net] Adding ipv4 gateway route failed: %v:%v", out, err) //nolint
+				log.Printf("[net] Adding ipv4 gateway route failed: %v:%v", out, err)
 			}
 		} else {
 			// netsh interface ipv6 add route $subnetV6 $hostInterfaceAlias "::"
 			netshV6DefaultRoute := fmt.Sprintf(netRouteCmd, "ipv6", prefix, ifName, ipv6DefaultHop)
 			if out, err = nm.plClient.ExecuteCommand(netshV6DefaultRoute); err != nil {
-				log.Printf("[net] Adding ipv6 default route failed: %v:%v", out, err) //nolint
+				log.Printf("[net] Adding ipv6 default route failed: %v:%v", out, err)
 			}
 
 			// netsh interface ipv6 add route $subnetV6 $hostInterfaceAlias $gatewayV6
 			netshV6GatewayRoute := fmt.Sprintf(netRouteCmd, "ipv6", prefix, ifName, gateway)
 			if out, err = nm.plClient.ExecuteCommand(netshV6GatewayRoute); err != nil {
-				log.Printf("[net] Adding ipv6 gateway route failed: %v:%v", out, err) //nolint
+				log.Printf("[net] Adding ipv6 gateway route failed: %v:%v", out, err)
 			}
 		}
 	}
