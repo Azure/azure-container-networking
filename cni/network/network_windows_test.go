c371e772b5522b085b5d85b3f38d20928dc59f90
jpayne3506 <johnpayne@microsoft.com>
2023-04-20 14:42:51 -0700

    Fix Lint

--- cni/network/network_windows_test.go
+++ cni/network/network_windows_test.go
@@ -248,7 +248,8 @@ func TestSetPoliciesFromNwCfg(t *testing.T) {
 				    },
 			    },
 		    },
-	    },
+	    }
+	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
