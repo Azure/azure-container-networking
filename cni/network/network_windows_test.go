75d325d2bd9a24bfef74f6689f3f0c405e637840
jpayne3506 <johnpayne@microsoft.com>
2023-04-20 14:07:16 -0700

    Fix Lint

--- cni/network/network_windows_test.go
+++ cni/network/network_windows_test.go
@@ -245,11 +245,10 @@ func TestSetPoliciesFromNwCfg(t *testing.T) {
 							HostPort:      44000,
 							ContainerPort: 80,
 						},
-				},
-			},
-		},
-	}
-
+				    },
+			    },
+		    },
+	    },
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
