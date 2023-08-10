a519a226f8ee3cbdfba756fb55fa54fc4aeb3706
aggarwal0009 <127549148+aggarwal0009@users.noreply.github.com>
2023-08-10 00:52:44 +0000

    removed unnecessary helper func from crd client

--- crd/multitenantpodnetworkconfig/client.go
+++ crd/multitenantpodnetworkconfig/client.go
@@ -16,7 +16,6 @@ import (
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 // Scheme is a runtime scheme containing the client-go scheme and the MultitenantPodNetworkConfig scheme.
@@ -103,59 +102,4 @@ func (c *Client) Get(ctx context.Context, key types.NamespacedName) (*v1alpha1.M
 	multitenantPodNetworkConfig := &v1alpha1.MultitenantPodNetworkConfig{}
 	err := c.cli.Get(ctx, key, multitenantPodNetworkConfig)
 	return multitenantPodNetworkConfig, errors.Wrapf(err, "failed to get mpnc %v", key)
-}
-
-// PatchSpec performs a server-side patch of the passed MultitenantPodNetworkConfigSpec to the MultitenantPodNetworkConfig specified by the NamespacedName.
-func (c *Client) PatchSpec(ctx context.Context, key types.NamespacedName, spec *v1alpha1.MultitenantPodNetworkConfigSpec, fieldManager string) (*v1alpha1.MultitenantPodNetworkConfig, error) {
-	obj := genPatchSkel(key)
-	obj.Spec = *spec
-	if err := c.cli.Patch(ctx, obj, client.Apply, client.ForceOwnership, client.FieldOwner(fieldManager)); err != nil {
-		return nil, errors.Wrap(err, "failed to patch mpnc")
-	}
-	return obj, nil
-}
-
-// UpdateSpec does a fetch, deepcopy, and update of the MultitenantPodNetworkConfig with the passed spec.
-// Deprecated: UpdateSpec is deprecated and usage should migrate to PatchSpec.
-func (c *Client) UpdateSpec(ctx context.Context, key types.NamespacedName, spec *v1alpha1.MultitenantPodNetworkConfigSpec) (*v1alpha1.MultitenantPodNetworkConfig, error) {
-	mpnc, err := c.Get(ctx, key)
-	if err != nil {
-		return nil, errors.Wrap(err, "failed to get mpnc")
-	}
-	spec.DeepCopyInto(&mpnc.Spec)
-	if err := c.cli.Update(ctx, mpnc); err != nil {
-		return nil, errors.Wrap(err, "failed to update mpnc")
-	}
-	return mpnc, nil
-}
-
-// SetOwnerRef sets the controller of the MultitenantPodNetworkConfig to the given object atomically, using HTTP Patch.
-// Deprecated: SetOwnerRef is deprecated, use the more correctly named SetControllerRef.
-func (c *Client) SetOwnerRef(ctx context.Context, key types.NamespacedName, owner metav1.Object, fieldManager string) (*v1alpha1.MultitenantPodNetworkConfig, error) {
-	return c.SetControllerRef(ctx, key, owner, fieldManager)
-}
-
-// SetControllerRef sets the controller of the MultitenantPodNetworkConfig to the given object atomically, using HTTP Patch.
-func (c *Client) SetControllerRef(ctx context.Context, key types.NamespacedName, owner metav1.Object, fieldManager string) (*v1alpha1.MultitenantPodNetworkConfig, error) {
-	obj := genPatchSkel(key)
-	if err := ctrlutil.SetControllerReference(owner, obj, Scheme); err != nil {
-		return nil, errors.Wrapf(err, "failed to set controller reference for mpnc")
-	}
-	if err := c.cli.Patch(ctx, obj, client.Apply, client.ForceOwnership, client.FieldOwner(fieldManager)); err != nil {
-		return nil, errors.Wrapf(err, "failed to patch mpnc")
-	}
-	return obj, nil
-}
-
-func genPatchSkel(key types.NamespacedName) *v1alpha1.MultitenantPodNetworkConfig {
-	return &v1alpha1.MultitenantPodNetworkConfig{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: v1alpha1.GroupVersion.String(),
-			Kind:       "MultitenantPodNetworkConfig",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      key.Name,
-			Namespace: key.Namespace,
-		},
-	}
 }
