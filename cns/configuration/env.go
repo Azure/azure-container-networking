099d0583341db65a78151332f2e3045a79134f83
Evan Baker <rbtr@users.noreply.github.com>
2023-08-14 23:48:30 +0000

    add swift v2 config based on node label
    Signed-off-by: Evan Baker <rbtr@users.noreply.github.com>

--- cns/configuration/env.go
+++ cns/configuration/env.go
@@ -11,6 +11,8 @@ const (
 	EnvNodeName = "NODENAME"
 	// EnvNodeIP is the IP of the node running this CNS binary
 	EnvNodeIP = "NODE_IP"
+	// LabelSwiftV2 is the Node label for Swift V2
+	LabelSwiftV2 = "swiftv2" // TODO(rbtr): update with real label
 )
 
 // ErrNodeNameUnset indicates the the $EnvNodeName variable is unset in the environment.
