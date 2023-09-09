76d370be8712b9c389d461fa66a15060fbeb5bb2
paulyufan2 <paulyu01@outlook.com>
2023-09-08 23:22:07 -0400

    network package zap logger

---
+++ network/log/logger.go
@@ -0,0 +1,36 @@
+package log
+
+import (
+	"os"
+
+	"github.com/Azure/azure-container-networking/zaplog"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	maxLogFileSizeInMb = 5
+	maxLogFileCount    = 8
+)
+
+var (
+	loggerName string
+	loggerFile string
+)
+
+var LoggerCfg = &zaplog.Config{
+	Level:       zapcore.DebugLevel,
+	LogPath:     loggerFile,
+	MaxSizeInMB: maxLogFileSizeInMb,
+	MaxBackups:  maxLogFileCount,
+	Name:        loggerName,
+}
+
+func InitZapLogNet(loggerName, loggerFile string) *zap.Logger {
+	LoggerCfg.Name = loggerName
+	LoggerCfg.LogPath = LogPath + loggerFile
+	logger := zaplog.InitZapLog(LoggerCfg)
+
+	logger = logger.With(zap.Int("pid", os.Getpid()))
+	return logger
+}
