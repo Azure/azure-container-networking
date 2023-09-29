995766a12d27678f591eb9e9c4089d3e2adf8109
paulyufan2 <paulyu01@outlook.com>
2023-09-29 15:38:58 -0400

    fix v6 internet connectivity

--- test/internal/datapath/datapath_win.go
+++ test/internal/datapath/datapath_win.go
@@ -15,7 +15,7 @@ import (
 	restclient "k8s.io/client-go/rest"
 )
 
-var ipv6PrefixPolicy = []string{"powershell", "-c", "curl.exe", "-6", "-v", "www.bing.com"}
+var ipv6PrefixPolicy = []string{"powershell", "-c", "curl.exe", "-6", "-v", "www.bing.com", "--head"}
 
 func podTest(ctx context.Context, clientset *kubernetes.Clientset, srcPod *apiv1.Pod, cmd []string, rc *restclient.Config, passFunc func(string) error) error {
 	logrus.Infof("podTest() - %v %v", srcPod.Name, cmd)
@@ -197,7 +197,7 @@ func WindowsPodToInternet(ctx context.Context, clientset *kubernetes.Clientset, 
 	}
 
 	// test Invoke-WebRequest an URL by IPv6 address on one pod
-	// command is: C:\inetpub\wwwroot> powershell curl.exe -6 -v www.bing.com
+	// command is: C:\inetpub\wwwroot> powershell -c curl.exe -6 -v www.bing.com
 	// then return *   Trying [2620:1ec:c11::200]:80...
 	//              HTTP/1.1 200 OK
 	if len(secondPod.Status.PodIPs) > 1 {
@@ -216,7 +216,6 @@ func WindowsPodToInternet(ctx context.Context, clientset *kubernetes.Clientset, 
 
 func webRequestPassedWindows(output string) error {
 	const searchString = "200 OK"
-	logrus.Infof("output is %s", output)
 	if strings.Contains(output, searchString) {
 		return nil
 	}
