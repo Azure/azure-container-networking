8a8b3252ebc26b17cf066b3a51c384a4e8a2859b
Paul Yu <129891899+paulyufan2@users.noreply.github.com>
2023-10-13 15:08:58 -0400

    [CNI] zap logger for platform package (#2233)
    * zap logger for platform package

--- cni/network/plugin/main.go
+++ cni/network/plugin/main.go
@@ -70,6 +70,7 @@ func rootExecute() error {
 			InterfaceDetails: telemetry.InterfaceInfo{},
 			BridgeDetails:    telemetry.BridgeInfo{},
 			Version:          version,
+			Logger:           logger,
 		},
 	}
 
@@ -95,7 +96,8 @@ func rootExecute() error {
 		cniReport.GetReport(pluginName, version, ipamQueryURL)
 
 		var upTime time.Time
-		upTime, err = platform.GetLastRebootTime()
+		p := platform.NewExecClient(logger)
+		upTime, err = p.GetLastRebootTime()
 		if err == nil {
 			cniReport.VMUptime = upTime.Format("2006-01-02 15:04:05")
 		}
