2212ffedcc664f42babd249f915f5fcdf976ae4d
paulyufan2 <paulyu01@outlook.com>
2024-04-16 12:25:22 -0400

    add UTs to launch server with different combinations

---
+++ cns/restserver/v2/server_test.go
@@ -0,0 +1,110 @@
+package v2
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/Azure/azure-container-networking/cns/common"
+	"github.com/Azure/azure-container-networking/cns/fakes"
+	"github.com/Azure/azure-container-networking/cns/logger"
+	"github.com/Azure/azure-container-networking/cns/restserver"
+	acncommon "github.com/Azure/azure-container-networking/common"
+)
+
+// TestStartServices will test three scenarios:
+// 1. when customer provides -p option, make sure local server is running and server is using this port
+// 2. when customer does not provide -p or -c option, local server will start running with default port 10090
+// 3. when customer provides -c option, local server will this -c URL
+
+func TestStartServerWithCNSPort(t *testing.T) {
+	var err error
+
+	logger.InitLogger("testlogs", 0, 0, "./")
+	cnsPort := "8000"
+
+	// Create the service with -p 8000
+	if err = startService(cnsPort, ""); err != nil {
+		fmt.Printf("Failed to start CNS Service. Error: %v", err)
+		os.Exit(1)
+	}
+}
+
+// customer does not provide -c or -p command, then use default port 10090
+func TestStartServerWithNoInput(t *testing.T) {
+	var err error
+
+	logger.InitLogger("testlogs", 0, 0, "./")
+
+	if err = startService("", ""); err != nil {
+		fmt.Printf("Failed to start CNS Service. Error: %v", err)
+		os.Exit(1)
+	}
+}
+
+func TestStartServerWithCNSURL(t *testing.T) {
+	var err error
+
+	logger.InitLogger("testlogs", 0, 0, "./")
+
+	// Create the service with -c "localhost:8000"
+	if err = startService("", "tcp://localhost:8000"); err != nil {
+		fmt.Printf("Failed to start CNS Service. Error: %v", err)
+		os.Exit(1)
+	}
+}
+
+// startService will return a URL that running server is using and check if sever can start
+// mock primaryVMIP as a fixed IP
+func startService(cnsPort, cnsURL string) error {
+	// Create the service.
+	config := common.ServiceConfig{}
+
+	nmagentClient := &fakes.NMAgentClientFake{}
+	service, err := restserver.NewHTTPRestService(&config, &fakes.WireserverClientFake{}, &fakes.WireserverProxyFake{}, nmagentClient, nil, nil, nil)
+	if err != nil {
+		return err
+	}
+	service.Name = "cns-test-server"
+
+	service.SetOption(acncommon.OptCnsPort, cnsPort)
+	service.SetOption(acncommon.OptCnsURL, cnsURL)
+
+	if service != nil {
+		config.Server.PrimaryInterfaceIP = "localhost"
+
+		err = service.Init(&config)
+		if err != nil {
+			logger.Errorf("Failed to Init CNS, err:%v.\n", err)
+			return err
+		}
+
+		err = service.Start(&config)
+		if err != nil {
+			logger.Errorf("Failed to start CNS, err:%v.\n", err)
+			return err
+		}
+	}
+
+	if cnsPort == "" {
+		cnsPort = "10090"
+	}
+	// check if we can reach this URL
+	urls := "localhost:" + cnsPort
+
+	if cnsURL != "" {
+		u, _ := url.Parse(cnsURL)
+		port := u.Port()
+		urls = "localhost:" + port
+	}
+
+	_, err = net.DialTimeout("tcp", urls, 10*time.Millisecond)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
