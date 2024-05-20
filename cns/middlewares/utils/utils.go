05d2c789fa99f35323e5918620c4c1b034a5ddea
bohuini <bohuini@microsoft.com>
2024-05-20 00:25:27 -0700

    Fix Lint issue: Wrap error

--- cns/middlewares/utils/utils.go
+++ cns/middlewares/utils/utils.go
@@ -1,19 +1,20 @@
 package utils
 
 import (
-	"fmt"
 	"net/netip"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
-// ParseCIDRs parses the comma separated list of CIDRs and returns the IPv4 and IPv6 CIDRs.
+// ParseCIDRs parses the comma-separated list of CIDRs and returns the IPv4 and IPv6 CIDRs.
 func ParseCIDRs(cidrs string) (v4IPs, v6IPs []string, err error) {
 	v4IPs = []string{}
 	v6IPs = []string{}
 	for _, cidr := range strings.Split(cidrs, ",") {
 		p, err := netip.ParsePrefix(cidr)
 		if err != nil {
-			return nil, nil, fmt.Errorf("failed to parse CIDR %s : %w", cidr, err)
+			return nil, nil, errors.Wrapf(err, "failed to parse CIDR %s", cidr)
 		}
 		ip := p.Addr()
 		if ip.Is4() {
@@ -29,7 +30,7 @@ func ParseCIDRs(cidrs string) (v4IPs, v6IPs []string, err error) {
 func ParseIPAndPrefix(primaryIP string) (string, int, error) {
 	p, err := netip.ParsePrefix(primaryIP)
 	if err != nil {
-		return "", 0, fmt.Errorf("failed to parse primaryIP %s: %w", primaryIP, err)
+		return "", 0, errors.Wrapf(err, "failed to parse primaryIP %s", primaryIP)
 	}
 	ip := p.Addr().String()
 	prefixSize := p.Bits()
