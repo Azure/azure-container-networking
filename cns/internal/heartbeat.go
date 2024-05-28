4d38aae405019b6292a7bc8c61cd0bdd82cf2f94
Zetao Zhuang <zetaozhuang@microsoft.com>
2024-05-28 13:24:24 -0700

    fix bug

--- cns/internal/heartbeat.go
+++ cns/internal/heartbeat.go
@@ -17,17 +17,17 @@ import (
 func SendHeartBeat(ctx context.Context, heartbeatIntervalInMins int, homeAzMonitor *restserver.HomeAzMonitor) {
 	ticker := time.NewTicker(time.Minute * time.Duration(heartbeatIntervalInMins))
 	defer ticker.Stop()
-	metric := aitelemetry.Metric{
-		Name: logger.HeartBeatMetricStr,
-		// This signifies 1 heartbeat is sent. Sum of this metric will give us number of heartbeats received
-		Value:            1.0,
-		CustomDimensions: make(map[string]string),
-	}
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
+			metric := aitelemetry.Metric{
+				Name: logger.HeartBeatMetricStr,
+				// This signifies 1 heartbeat is sent. Sum of this metric will give us number of heartbeats received
+				Value:            1.0,
+				CustomDimensions: make(map[string]string),
+			}
 			getHomeAzResp := homeAzMonitor.GetHomeAz(ctx)
 			if getHomeAzResp.Response.ReturnCode == types.Success {
 				metric.CustomDimensions[logger.IsAZRSupportedStr] = strconv.FormatBool(getHomeAzResp.HomeAzResponse.IsSupported)
