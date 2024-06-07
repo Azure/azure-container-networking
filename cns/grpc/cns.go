dc34c926abfa80b7f8975802bb95f2fe6cf309de
Kevin <t-kcamacho@microsoft.com>
2024-06-07 14:55:46 +0000

    Resolved Concflicts

--- cns/grpc/cns.go
+++ cns/grpc/cns.go
@@ -15,14 +15,14 @@ type CNS struct {
 	State  *restserver.HTTPRestService
 }
 
-func (s *Server) SetOrchestratorInfo(ctx context.Context, req *pb.SetOrchestratorInfoRequest) (*pb.SetOrchestratorInfoResponse, error) {
-	s.Logger.Info("SetOrchestratorInfo called", zap.String("nodeID", req.NodeID), zap.String("orchestratorType", req.OrchestratorType))
-	// todo: Implement the logic 
+func (s *Server) SetOrchestratorInfo(_ context.Context, req *pb.SetOrchestratorInfoRequest) (*pb.SetOrchestratorInfoResponse, error) {
+	s.Logger.Info("SetOrchestratorInfo called", zap.String("nodeID", req.GetNodeID()), zap.String("orchestratorType", req.GetOrchestratorType()))
+	// todo: Implement the logic
 	return &pb.SetOrchestratorInfoResponse{}, nil
 }
 
-func (s *Server) GetNodeInfo(ctx context.Context, req *pb.NodeInfoRequest) (*pb.NodeInfoResponse, error) {
-	s.Logger.Info("GetNodeInfo called", zap.String("nodeID", req.NodeID))
-	// todo: Implement the logic 
+func (s *Server) GetNodeInfo(_ context.Context, req *pb.NodeInfoRequest) (*pb.NodeInfoResponse, error) {
+	s.Logger.Info("GetNodeInfo called", zap.String("nodeID", req.GetNodeID()))
+	// todo: Implement the logic
 	return &pb.NodeInfoResponse{}, nil
 }
