55d33b070671b4fa14de67daced81912e7728788
paulyufan2 <paulyu01@outlook.com>
2024-06-20 12:11:32 -0400

    fix a comment linter issue

--- cns/middlewares/k8sSwiftV2_windows.go
+++ cns/middlewares/k8sSwiftV2_windows.go
@@ -5,11 +5,10 @@ import (
 	"github.com/Azure/azure-container-networking/cns/logger"
 )
 
-// setRoutes sets the routes for podIPInfo used in SWIFT V2 scenario. 
 // for AKS L1VH, do not set default route on infraNIC to avoid customer pod reaching all infra vnet services
-// default route is set for secondary interface NIC
+// default route is set for secondary interface NIC(i.e,delegatedNIC)
 func (k *K8sSWIFTv2Middleware) setRoutes(podIPInfo *cns.PodIpInfo) error {
-	logger.Printf("[SWIFTv2Middleware] setRoutes: only skipDefaultRoutes for InfraNIC")
+	logger.Printf("[SWIFTv2Middleware] setRoutes: only set skipDefaultRoutes flag to true for InfraNIC")
 	if podIPInfo.NICType == cns.InfraNIC {
 		podIPInfo.SkipDefaultRoutes = true
 	}
