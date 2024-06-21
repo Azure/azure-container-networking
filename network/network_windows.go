5a1eb43f51baa9534f111b2a0995d0aebf7677b4
paulyufan2 <paulyu01@outlook.com>
2024-06-21 15:07:29 -0400

    add primaryip and gateway on CNS

--- network/network_windows.go
+++ network/network_windows.go
@@ -304,6 +304,7 @@ func (nm *networkManager) configureHcnNetwork(nwInfo *EndpointInfo, extIf *exter
 
 	// Populate subnets.
 	for _, subnet := range nwInfo.Subnets {
+		logger.Info("NextHop is", zap.String("NextHop", subnet.Gateway.String()))
 		hnsSubnet := hcn.Subnet{
 			IpAddressPrefix: subnet.Prefix.String(),
 			// Set the Gateway route
