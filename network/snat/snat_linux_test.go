bfd9e796d0041046e4bff7b72d31712f13c79578
QxBytes <alexanderlew@microsoft.com>
2024-12-02 14:27:27 -0800

    modify snat client to accept interfaces for unit testing

--- network/snat/snat_linux_test.go
+++ network/snat/snat_linux_test.go
@@ -4,11 +4,27 @@ import (
 	"os"
 	"testing"
 
-	"github.com/Azure/azure-container-networking/iptables"
+	"github.com/Azure/azure-container-networking/netio"
 	"github.com/Azure/azure-container-networking/netlink"
 )
 
 var anyInterface = "dummy"
+
+type mockIPTablesClient struct {
+}
+
+func (c mockIPTablesClient) InsertIptableRule(version, tableName, chainName, match, target string) error {
+	return nil
+}
+func (c mockIPTablesClient) AppendIptableRule(version, tableName, chainName, match, target string) error {
+	return nil
+}
+func (c mockIPTablesClient) DeleteIptableRule(version, tableName, chainName, match, target string) error {
+	return nil
+}
+func (c mockIPTablesClient) CreateChain(version, tableName, chainName string) error {
+	return nil
+}
 
 func TestMain(m *testing.M) {
 	exitCode := m.Run()
@@ -18,16 +34,22 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestAllowInboundFromHostToNC(t *testing.T) {
-	nl := netlink.NewNetlink()
-	iptc := iptables.NewClient()
-	client := &Client{
+func GetTestClient(nl netlink.NetlinkInterface, iptc ipTablesClient, nio netio.NetIOInterface) *Client {
+	return &Client{
 		SnatBridgeIP:          "169.254.0.1/16",
 		localIP:               "169.254.0.4/16",
 		containerSnatVethName: anyInterface,
 		netlink:               nl,
 		ipTablesClient:        iptc,
+		netioClient:           nio,
 	}
+}
+
+func TestAllowInboundFromHostToNC(t *testing.T) {
+	nl := netlink.NewMockNetlink(false, "")
+	iptc := &mockIPTablesClient{}
+	nio := netio.NewMockNetIO(false, 0)
+	client := GetTestClient(nl, iptc, nio)
 
 	if err := nl.AddLink(&netlink.DummyLink{
 		LinkInfo: netlink.LinkInfo{
@@ -65,18 +87,18 @@ func TestAllowInboundFromHostToNC(t *testing.T) {
 	if err := nl.DeleteLink(SnatBridgeName); err != nil {
 		t.Errorf("Error removing snat bridge: %v", err)
 	}
+
+	client.netioClient = netio.NewMockNetIO(true, 1)
+	if err := client.AllowInboundFromHostToNC(); err == nil {
+		t.Errorf("Expected error when interface not found in allow host to nc but got nil")
+	}
 }
 
 func TestAllowInboundFromNCToHost(t *testing.T) {
-	nl := netlink.NewNetlink()
-	iptc := iptables.NewClient()
-	client := &Client{
-		SnatBridgeIP:          "169.254.0.1/16",
-		localIP:               "169.254.0.4/16",
-		containerSnatVethName: anyInterface,
-		netlink:               nl,
-		ipTablesClient:        iptc,
-	}
+	nl := netlink.NewMockNetlink(false, "")
+	iptc := &mockIPTablesClient{}
+	nio := netio.NewMockNetIO(false, 0)
+	client := GetTestClient(nl, iptc, nio)
 
 	if err := nl.AddLink(&netlink.DummyLink{
 		LinkInfo: netlink.LinkInfo{
@@ -113,5 +135,10 @@ func TestAllowInboundFromNCToHost(t *testing.T) {
 	}
 	if err := nl.DeleteLink(SnatBridgeName); err != nil {
 		t.Errorf("Error removing snat bridge: %v", err)
+	}
+
+	client.netioClient = netio.NewMockNetIO(true, 1)
+	if err := client.AllowInboundFromNCToHost(); err == nil {
+		t.Errorf("Expected error when interface not found in allow nc to host but got nil")
 	}
 }
