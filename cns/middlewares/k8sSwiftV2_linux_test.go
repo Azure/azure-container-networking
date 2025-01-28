e9d2717e87e6dcbc203069fd731930f88ea6c0a3
Paul Yu <paulyu01@outlook.com>
2025-01-28 14:55:27 -0500

    ut fix

--- cns/middlewares/k8sSwiftV2_linux_test.go
+++ cns/middlewares/k8sSwiftV2_linux_test.go
@@ -343,10 +343,10 @@ func TestSetRoutesSuccess(t *testing.T) {
 		} else {
 			assert.Equal(t, ipInfo.SkipDefaultRoutes, false)
 		}
-
 	}
+
 	for i := range podIPInfo {
-		reflect.DeepEqual(t, podIPInfo[i].Routes, desiredPodIPInfo[i].Routes)
+		reflect.DeepEqual(podIPInfo[i].Routes, desiredPodIPInfo[i].Routes)
 	}
 }
 
@@ -382,7 +382,7 @@ func TestAddRoutes(t *testing.T) {
 	middleware := K8sSWIFTv2Middleware{Cli: mock.NewClient()}
 	cidrs := []string{"10.0.0.0/24", "20.0.0.0/24"}
 	gatewayIP := "192.168.1.1"
-	routes := middleware.addRoutes(cidrs, gatewayIP)
+	routes := middleware.AddRoutes(cidrs, gatewayIP)
 	expectedRoutes := []cns.Route{
 		{
 			IPAddress:        "10.0.0.0/24",
