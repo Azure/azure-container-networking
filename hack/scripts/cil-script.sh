a8c4cf966a0534c17a6af814359dd7d876b77d62
Isaiah Raya <isaiahraya@microsoft.com>
2025-05-09 18:55:57 +0000

    updated ipam.operator.clusterPoolIPv4PodCIDRList to be equal to vnet cidr range and set node count to 1

--- hack/scripts/cil-script.sh
+++ hack/scripts/cil-script.sh
@@ -16,6 +16,7 @@ for unique in $sufixes; do
         AZCLI=az REGION=westus2 SUB=$SUB \
         CLUSTER=${clusterPrefix}-${unique} \
         KUBE_PROXY_JSON_PATH=./kube-proxy.json \
+        NODE_COUNT=1 \
         VNET_PREFIX=10.${unique}.0.0/16 \
         NODE_SUBNET_PREFIX=10.${unique}.1.0/24 \
         POD_SUBNET_PREFIX=10.${unique}.2.0/24
@@ -28,7 +29,7 @@ for unique in $sufixes; do
         --set cluster.name=${clusterPrefix}-${unique} \
         --set azure.resourceGroup=${clusterPrefix}-${unique}-rg \
         --set cluster.id=${unique} \
-        --set ipam.operator.clusterPoolIPv4PodCIDRList='{192.'${unique}'0.0.0/16}' \
+        --set ipam.operator.clusterPoolIPv4PodCIDRList='{10.'${unique}'.2.0/24}' \
         --set hubble.enabled=false \
         --set envoy.enabled=false
     fi
