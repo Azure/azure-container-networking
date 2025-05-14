cd243156571be231f40daa18eb648ca6f0a930e5
Sheyla Trudo <sheyla.trudo@gmail.com>
2025-05-14 14:54:23 -0700

    fixup! Use Signed Binaries for Docker Build

--- .pipelines/build/scripts/cni.sh
+++ .pipelines/build/scripts/cni.sh
@@ -6,14 +6,67 @@ ls -la
 mkdir -p "$OUT_DIR"/files
 mkdir -p "$OUT_DIR"/bins
 
-#CNI_BUILD_DIR=$(mktemp -d -p "$GEN_DIR")
+export GOOS=$OS
+export GOARCH=$ARCH
+export CGO_ENABLED=0 
+
+CNI_BUILD_DIR="$BUILD_DIR"/cni
+STATELESS_CNI_BUILD_DIR="$CNI_BUILD_DIR"/stateless
+CNI_MULTITENANCY_BUILD_DIR="$BUILD_DIR"/cni-multitenancy
+CNI_MULTITENANCY_TRANSPARENT_VLAN_BUILD_DIR="$BUILD_DIR"/cni-multitenancy-transparent-vlan
+CNI_SWIFT_BUILD_DIR="$BUILD_DIR"/cni-swift
+CNI_OVERLAY_BUILD_DIR="$BUILD_DIR"/cni-overlay
+CNI_BAREMETAL_BUILD_DIR="$BUILD_DIR"/cni-baremetal
+CNI_DUALSTACK_BUILD_DIR="$BUILD_DIR"/cni-dualstack
+
+CNI_TEMP_DIR=$(mktemp -d -p "$GEN_DIR")
+
+CNI_NET_DIR="$REPO_ROOT"/cni/network/plugin
+pushd "$CNI_NET_DIR"
+  go build -v -a -trimpath \
+    -o "$OUT_DIR"/bins/azure-vnet \
+    -ldflags "-X main.version="$CNI_VERSION"" \
+    -gcflags="-dwarflocationlists=true" \
+    ./main.go
+popd
+
+STATELESS_CNI_NET_DIR="$REPO_ROOT"/cni/network/stateless
+pushd "$STATELESS_CNI_NET_DIR"
+  go build -v -a -trimpath \
+    -o "$OUT_DIR"/bins/azure-vnet-stateless \
+    -ldflags "-X main.version="$CNI_VERSION"" \
+    -gcflags="-dwarflocationlists=true" \
+    ./main.go
+popd
+
+CNI_IPAM_DIR="$REPO_ROOT"/cni/ipam/plugin
+pushd "$CNI_IPAM_DIR"
+  go build -v -a -trimpath \
+    -o "$OUT_DIR"/bins/azure-vnet-ipam \
+    -ldflags "-X main.version="$CNI_VERSION"" \
+    -gcflags="-dwarflocationlists=true" \
+    ./main.go
+popd
+
+CNI_IPAMV6_DIR="$REPO_ROOT"/cni/ipam/pluginv6
+pushd "$CNI_IPAMV6_DIR"
+  go build -v -a -trimpath \
+    -o "$OUT_DIR"/bins/azure-vnet-ipamv6 
+    -ldflags "-X main.version="$CNI_VERSION"" \
+    -gcflags="-dwarflocationlists=true" \
+    ./main.go
+popd
+
+CNI_TELEMETRY_DIR="$REPO_ROOT"/cni/telemetry/service
+pushd "$CNI_TELEMETRY_DIR"
+  go build -v -a -trimpath \
+    -o "$OUT_DIR"/bins/azure-vnet-telemetry \
+    -ldflags "-X main.version="$CNI_VERSION" -X "$CNI_AI_PATH"="$CNI_AI_ID"" \
+    -gcflags="-dwarflocationlists=true" \
+   ./telemetrymain.go
+popd
 
 pushd "$REPO_ROOT"/cni
-  GOOS=$OS CGO_ENABLED=0 go build -a -o "$OUT_DIR"/bins/azure-vnet -trimpath -ldflags "-X main.version="$VERSION"" -gcflags="-dwarflocationlists=true" network/plugin/main.go
-  GOOS=$OS CGO_ENABLED=0 go build -a -o "$OUT_DIR"/bins/azure-vnet-telemetry -trimpath -ldflags "-X main.version="$VERSION" -X "$CNI_AI_PATH"="$CNI_AI_ID"" -gcflags="-dwarflocationlists=true" ../telemetry/service/telemetrymain.go
-  GOOS=$OS CGO_ENABLED=0 go build -a -o "$OUT_DIR"/bins/azure-vnet-ipam -trimpath -ldflags "-X main.version="$VERSION"" -gcflags="-dwarflocationlists=true" ipam/plugin/main.go
-  GOOS=$OS CGO_ENABLED=0 go build -a -o "$OUT_DIR"/bins/azure-vnet-stateless -trimpath -ldflags "-X main.version="$VERSION"" -gcflags="-dwarflocationlists=true" network/stateless/main.go
-
   cp azure-$OS.conflist "$OUT_DIR"/files/azure.conflist
   cp azure-$OS-swift.conflist "$OUT_DIR"/files/azure-swift.conflist
   cp azure-linux-multitenancy-transparent-vlan.conflist "$OUT_DIR"/files/azure-multitenancy-transparent-vlan.conflist
@@ -22,12 +75,19 @@ pushd "$REPO_ROOT"/cni
   cp azure-$OS-multitenancy.conflist "$OUT_DIR"/files/multitenancy.conflist
   cp ../telemetry/azure-vnet-telemetry.config "$OUT_DIR"/files/azure-vnet-telemetry.config
   sha256sum * > sum.txt
-  gzip --verbose --best --recursive "$OUT_DIR" && for f in *.gz; do mv -- "$f" "${f%%.gz}"; done
+  #gzip --verbose --best --recursive "$OUT_DIR" && for f in *.gz; do mv -- "$f" "${f%%.gz}"; done
 popd
 
-go mod download github.com/azure/azure-container-networking/dropgz@$DROPGZ_VERSION
-pushd "$GOPATH"/pkg/mod/github.com/azure/azure-container-networking/dropgz\@$DROPGZ_VERSION
+
+mkdir -p "$CNI_TEMP_DIR"
+GOPATH="$CNI_TEMP_DIR" go mod download github.com/azure/azure-container-networking/dropgz@$DROPGZ_VERSION
+
+pushd "$CNI_TEMP_DIR"/pkg/mod/github.com/azure/azure-container-networking/dropgz\@$DROPGZ_VERSION
   cp "$OUT_DIR"/files/* pkg/embed/fs/
   cp "$OUT_DIR"/bins/* pkg/embed/fs/
-  GOOS=$OS CGO_ENABLED=0 go build -a -o "$OUT_DIR"/bins/dropgz -trimpath -ldflags "-X github.com/Azure/azure-container-networking/dropgz/internal/buildinfo.Version="$VERSION"" -gcflags="-dwarflocationlists=true" main.go
+  go build -a \
+    -o "$OUT_DIR"/bins/dropgz \
+    -ldflags "-X github.com/Azure/azure-container-networking/dropgz/internal/buildinfo.Version="$VERSION"" \
+    -gcflags="-dwarflocationlists=true" \
+    ./main.go
 popd
