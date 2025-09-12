f3a98a5d31082ce3fd77ad202973d919d81d2b1a
Paul Johnston <johnstonpaul801@gmail.com>
2025-09-12 21:36:40 +0000

    initializeMultiTenantController

--- cns/service/main.go
+++ cns/service/main.go
@@ -36,8 +36,6 @@ import (
 	loggerv2 "github.com/Azure/azure-container-networking/cns/logger/v2"
 	"github.com/Azure/azure-container-networking/cns/metric"
 	"github.com/Azure/azure-container-networking/cns/middlewares"
-	"github.com/Azure/azure-container-networking/cns/multitenantcontroller"
-	"github.com/Azure/azure-container-networking/cns/multitenantcontroller/multitenantoperator"
 	"github.com/Azure/azure-container-networking/cns/restserver"
 	restserverv2 "github.com/Azure/azure-container-networking/cns/restserver/v2"
 	cnipodprovider "github.com/Azure/azure-container-networking/cns/stateprovider/cni"
@@ -876,7 +874,7 @@ func main() {
 	// Initialize multi-tenant controller if the CNS is running in MultiTenantCRD mode.
 	// It must be started before we start HTTPRemoteRestService.
 	if config.ChannelMode == cns.MultiTenantCRD {
-		err = InitializeMultiTenantController(rootCtx, httpRemoteRestService, *cnsconfig)
+		err = initializeMultiTenantController(rootCtx, httpRemoteRestService, *cnsconfig)
 		if err != nil {
 			logger.Errorf("Failed to start multiTenantController, err:%v.\n", err)
 			return
@@ -1183,79 +1181,6 @@ func pollNodeInfoCRDAndUpdatePlugin(ctx context.Context, zlog *zap.Logger, plugi
 			}
 		}
 	}
-}
-
-func InitializeMultiTenantController(ctx context.Context, httpRestService cns.HTTPService, cnsconfig configuration.CNSConfig) error {
-	var multiTenantController multitenantcontroller.RequestController
-	kubeConfig, err := ctrl.GetConfig()
-	kubeConfig.UserAgent = fmt.Sprintf("azure-cns-%s", version)
-	if err != nil {
-		return err
-	}
-
-	// convert interface type to implementation type
-	httpRestServiceImpl, ok := httpRestService.(*restserver.HTTPRestService)
-	if !ok {
-		logger.Errorf("Failed to convert interface httpRestService to implementation: %v", httpRestService)
-		return fmt.Errorf("Failed to convert interface httpRestService to implementation: %v",
-			httpRestService)
-	}
-
-	// Set orchestrator type
-	orchestrator := cns.SetOrchestratorTypeRequest{
-		OrchestratorType: cns.Kubernetes,
-	}
-	httpRestServiceImpl.SetNodeOrchestrator(&orchestrator)
-
-	// Create multiTenantController.
-	multiTenantController, err = multitenantoperator.New(httpRestServiceImpl, kubeConfig)
-	if err != nil {
-		logger.Errorf("Failed to create multiTenantController:%v", err)
-		return err
-	}
-
-	// Wait for multiTenantController to start.
-	go func() {
-		for {
-			if err := multiTenantController.Start(ctx); err != nil {
-				logger.Errorf("Failed to start multiTenantController: %v", err)
-			} else {
-				logger.Printf("Exiting multiTenantController")
-				return
-			}
-
-			// Retry after 1sec
-			time.Sleep(time.Second)
-		}
-	}()
-	for {
-		if multiTenantController.IsStarted() {
-			logger.Printf("MultiTenantController is started")
-			break
-		}
-
-		logger.Printf("Waiting for multiTenantController to start...")
-		time.Sleep(time.Millisecond * 500)
-	}
-
-	// TODO: do we need this to be running?
-	logger.Printf("Starting SyncHostNCVersion")
-	go func() {
-		// Periodically poll vfp programmed NC version from NMAgent
-		tickerChannel := time.Tick(time.Duration(cnsconfig.SyncHostNCVersionIntervalMs) * time.Millisecond)
-		for {
-			select {
-			case <-tickerChannel:
-				timedCtx, cancel := context.WithTimeout(ctx, time.Duration(cnsconfig.SyncHostNCVersionIntervalMs)*time.Millisecond)
-				httpRestServiceImpl.SyncHostNCVersion(timedCtx, cnsconfig.ChannelMode)
-				cancel()
-			case <-ctx.Done():
-				return
-			}
-		}
-	}()
-
-	return nil
 }
 
 // getPodInfoByIPProvider returns a PodInfoByIPProvider that reads endpoint state from the configured source
