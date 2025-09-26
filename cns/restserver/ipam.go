8825bb6aad568f91496198694a72d54a10f3b01a
jpayne3506 <payne.3506@gmail.com>
2025-09-26 14:25:23 -0500

    fix: Improper CNS log

--- cns/restserver/ipam.go
+++ cns/restserver/ipam.go
@@ -1061,7 +1061,7 @@ func (service *HTTPRestService) AssignAvailableIPConfigs(podInfo cns.PodInfo) ([
 		return podIPInfo, fmt.Errorf("not enough IPs available, waiting on Azure CNS to allocate more")
 	}
 
-	logger.Printf("[AssignDesiredIPConfigs] Successfully assigned IPs for pod %+v", podInfo)
+	logger.Printf("[AssignAvailableIPConfigs] Successfully assigned IPs for pod %+v", podInfo)
 	return podIPInfo, nil
 }
 
