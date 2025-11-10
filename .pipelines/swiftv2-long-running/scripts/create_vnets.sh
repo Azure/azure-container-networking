2c6d4de933c80034d8991509bc2971fdb7c8813b
sivakami <sivakamis@microsoft.com>
2025-11-09 22:07:45 -0800

    Stage 2 for long running cluster test pipeline.

--- .pipelines/swiftv2-long-running/scripts/create_vnets.sh
+++ .pipelines/swiftv2-long-running/scripts/create_vnets.sh
@@ -2,27 +2,22 @@
 set -e
 trap 'echo "[ERROR] Failed while creating VNets or subnets. Check Azure CLI logs above." >&2' ERR
 
-SUBSCRIPTION_ID=$1
+SUB_ID=$1
 LOCATION=$2
 RG=$3
+BUILD_ID=$4
 
-az account set --subscription "$SUBSCRIPTION_ID"
-
-# VNets and subnets
-VNET_A1="cx_vnet_a1"
-VNET_A2="cx_vnet_a2"
-VNET_A3="cx_vnet_a3"
-VNET_B1="cx_vnet_b1"
-
-A1_S1="10.10.1.0/24"
-A1_S2="10.10.2.0/24"
-A1_PE="10.10.100.0/24"
-
-A2_MAIN="10.11.1.0/24"
-
-A3_MAIN="10.12.1.0/24"
-
-B1_MAIN="10.20.1.0/24"
+# --- VNet definitions ---
+# Create customer vnets for two customers A and B.
+VNAMES=( "cx_vnet_a1" "cx_vnet_a2" "cx_vnet_a3" "cx_vnet_b1" )
+VCIDRS=( "10.10.0.0/16" "10.11.0.0/16" "10.12.0.0/16" "10.13.0.0/16" )
+NODE_SUBNETS=( "10.10.0.0/24" "10.11.0.0/24" "10.12.0.0/24" "10.13.0.0/24" )
+EXTRA_SUBNETS_LIST=( "s1 s2 pe" "s1" "s1" "s1" )
+EXTRA_CIDRS_LIST=( "10.10.1.0/24,10.10.2.0/24,10.10.3.0/24" \
+                   "10.11.1.0/24" \
+                   "10.12.1.0/24" \
+                   "10.13.1.0/24" )
+az account set --subscription "$SUB_ID"
 
 # -------------------------------
 # Verification functions
@@ -49,36 +44,36 @@ verify_subnet() {
   fi
 }
 
-# -------------------------------
-#  Create VNets and Subnets
-# -------------------------------
-# A1
-az network vnet create -g "$RG" -n "$VNET_A1" --address-prefix 10.10.0.0/16 --subnet-name s1 --subnet-prefix "$A1_S1" -l "$LOCATION" --output none \
- && echo "Created $VNET_A1 with subnet s1"
-az network vnet subnet create -g "$RG" --vnet-name "$VNET_A1" -n s2 --address-prefix "$A1_S2" --output none \
- && echo "Created $VNET_A1 with subnet s2"
-az network vnet subnet create -g "$RG" --vnet-name "$VNET_A1" -n pe --address-prefix "$A1_PE" --output none \
- && echo "Created $VNET_A1 with subnet pe"
-# Verify A1
-verify_vnet "$RG" "$VNET_A1"
-for sn in s1 s2 pe; do verify_subnet "$RG" "$VNET_A1" "$sn"; done
+# --- Loop over VNets ---
+for i in "${!VNAMES[@]}"; do
+    VNET=${VNAMES[$i]}
+    VNET_CIDR=${VCIDRS[$i]}
+    NODE_SUBNET_CIDR=${NODE_SUBNETS[$i]}
+    EXTRA_SUBNETS=${EXTRA_SUBNETS_LIST[$i]}
+    EXTRA_SUBNET_CIDRS=${EXTRA_CIDRS_LIST[$i]}
 
-# A2
-az network vnet create -g "$RG" -n "$VNET_A2" --address-prefix 10.11.0.0/16 --subnet-name s1 --subnet-prefix "$A2_MAIN" -l "$LOCATION" --output none \
- && echo "Created $VNET_A2 with subnet s1"
-verify_vnet "$RG" "$VNET_A2"
-verify_subnet "$RG" "$VNET_A2" "s1"
+    # Create VNet + subnets
+    make -C ./hack/aks swift-delegated-subnet-up \
+      AZCLI=az REGION=$LOCATION GROUP=$RG VNET=$VNET \
+      VNET_CIDR=$VNET_CIDR NODE_SUBNET_CIDR=$NODE_SUBNET_CIDR \
+      EXTRA_SUBNETS="$EXTRA_SUBNETS" EXTRA_SUBNET_CIDRS="$EXTRA_SUBNET_CIDRS" \
+      && echo "Created $VNET with subnets $EXTRA_SUBNETS"
 
-# A3
-az network vnet create -g "$RG" -n "$VNET_A3" --address-prefix 10.12.0.0/16 --subnet-name s1 --subnet-prefix "$A3_MAIN" -l "$LOCATION" --output none \
- && echo "Created $VNET_A3 with subnet s1"
-verify_vnet "$RG" "$VNET_A3"
-verify_subnet "$RG" "$VNET_A3" "s1"
+    verify_vnet "$RG" "$VNET"   # Verify VNet
 
-# B1
-az network vnet create -g "$RG" -n "$VNET_B1" --address-prefix 10.20.0.0/16 --subnet-name s1 --subnet-prefix "$B1_MAIN" -l "$LOCATION" --output none \
- && echo "Created $VNET_B1 with subnet s1"
-verify_vnet "$RG" "$VNET_B1"
-verify_subnet "$RG" "$VNET_B1" "s1"
+    # Loop over extra subnets to verify and create dummy clusters to delegate the pod subnets.
+    for PODSUBNET in $EXTRA_SUBNETS; do
+        verify_subnet "$RG" "$VNET" "$PODSUBNET"
+        if [[ "$PODSUBNET" == "pe" ]]; then
+            # Skip creating dummy cluster for private endpoint subnet.
+            continue
+        fi
+        cluster_name="${BUILD_ID}-${VNET}-${PODSUBNET}"
+        make -C ./hack/aks swiftv2-dummy-cluster-subnet-delegator-up \
+            AZCLI=az CLUSTER=$cluster_name GROUP=$RG REGION=$LOCATION \
+            SUB=$SUB_ID VNET=$VNET POD_SUBNET=$PODSUBNET \
+            && echo "Created dummy cluster for $VNET subnet $PODSUBNET"
+    done
+done
 
-echo " All VNets and subnets created and verified successfully."
+echo "All VNets and subnets created and verified successfully."
