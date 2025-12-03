2c63560006f786d0324a20fb42f403e43da2fd60
Santhosh Prabhu <sanpra1989@gmail.com>
2025-12-03 11:15:56 -0800

    fix: do not copy empty values into secondary IP configs

--- test/integration/cilium-nodesubnet/ipconfigupdate.go
+++ test/integration/cilium-nodesubnet/ipconfigupdate.go
@@ -100,12 +100,14 @@ func main() {
 		}
 
 		if primaryIPConfig != nil {
-			for i := 2; i <= secondaryConfigCount+1; i++ {
+			ipConfigurations = []interface{}{}
+			for i := 1; i <= secondaryConfigCount+1; i++ {
 				ipConfig := make(map[string]interface{})
 				for k, v := range primaryIPConfig {
+					fmt.Printf("%s %s\n", k, v)
 					// only the primary config needs loadBalancerBackendAddressPools. Azure doesn't allow
 					// secondary IP configs to be associated load balancer backend pools.
-					if k == "loadBalancerBackendAddressPools" {
+					if k == "loadBalancerBackendAddressPools" || v == nil {
 						continue
 					}
 					ipConfig[k] = v
@@ -117,6 +119,8 @@ func main() {
 					ipConfig["primary"] = false
 					usedIPConfigNames = append(usedIPConfigNames, ipConfigName)
 					secondaryConfigs = append(secondaryConfigs, ipConfig)
+				} else {
+					ipConfigurations = append(ipConfigurations, ipConfig)
 				}
 			}
 		}
