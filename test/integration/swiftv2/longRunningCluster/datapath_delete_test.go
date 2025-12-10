f7dc9496b2673e8edbed61b82ea960cfef032dc1
sivakami <sivakamis@microsoft.com>
2025-12-09 21:13:06 -0800

    Delete pod resources created for scale tests.

--- test/integration/swiftv2/longRunningCluster/datapath_delete_test.go
+++ test/integration/swiftv2/longRunningCluster/datapath_delete_test.go
@@ -5,8 +5,10 @@ package longRunningCluster
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
+	"github.com/Azure/azure-container-networking/test/integration/swiftv2/helpers"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
@@ -110,6 +112,60 @@ var _ = ginkgo.Describe("Datapath Delete Tests", func() {
 		err := DeleteAllScenarios(testScenarios)
 		gomega.Expect(err).To(gomega.BeNil(), "Failed to delete test scenarios")
 
-		ginkgo.By("Successfully deleted all test scenarios")
+		// Delete scale test resources
+		ginkgo.By("Deleting scale test resources")
+		scaleScenarios := []struct {
+			cluster  string
+			vnetName string
+			subnet   string
+			podCount int
+		}{
+			{cluster: "aks-1", vnetName: "cx_vnet_v1", subnet: "s1", podCount: 3},
+			{cluster: "aks-2", vnetName: "cx_vnet_v2", subnet: "s1", podCount: 2},
+		}
+
+		podIndex := 0
+		for _, scenario := range scaleScenarios {
+			kubeconfig := fmt.Sprintf("/tmp/%s.kubeconfig", scenario.cluster)
+			vnetShort := strings.TrimPrefix(scenario.vnetName, "cx_vnet_")
+			vnetShort = strings.ReplaceAll(vnetShort, "_", "-")
+			subnetNameSafe := strings.ReplaceAll(scenario.subnet, "_", "-")
+			pnName := fmt.Sprintf("pn-scale-%s-%s-%s", buildId, vnetShort, subnetNameSafe)
+			pniName := fmt.Sprintf("pni-scale-%s-%s-%s", buildId, vnetShort, subnetNameSafe)
+
+			// Delete pods
+			for j := 0; j < scenario.podCount; j++ {
+				podName := fmt.Sprintf("scale-pod-%d", podIndex)
+				ginkgo.By(fmt.Sprintf("Deleting scale test pod: %s from cluster %s", podName, scenario.cluster))
+				err := helpers.DeletePod(kubeconfig, pnName, podName)
+				if err != nil {
+					fmt.Printf("Warning: Failed to delete scale pod %s: %v\n", podName, err)
+				}
+				podIndex++
+			}
+
+			// Delete PodNetworkInstance
+			ginkgo.By(fmt.Sprintf("Deleting scale test PodNetworkInstance: %s from cluster %s", pniName, scenario.cluster))
+			err = helpers.DeletePodNetworkInstance(kubeconfig, pnName, pniName)
+			if err != nil {
+				fmt.Printf("Warning: Failed to delete scale test PNI %s: %v\n", pniName, err)
+			}
+
+			// Delete PodNetwork
+			ginkgo.By(fmt.Sprintf("Deleting scale test PodNetwork: %s from cluster %s", pnName, scenario.cluster))
+			err = helpers.DeletePodNetwork(kubeconfig, pnName)
+			if err != nil {
+				fmt.Printf("Warning: Failed to delete scale test PodNetwork %s: %v\n", pnName, err)
+			}
+
+			// Delete namespace
+			ginkgo.By(fmt.Sprintf("Deleting scale test namespace: %s from cluster %s", pnName, scenario.cluster))
+			err = helpers.DeleteNamespace(kubeconfig, pnName)
+			if err != nil {
+				fmt.Printf("Warning: Failed to delete scale test namespace %s: %v\n", pnName, err)
+			}
+		}
+
+		ginkgo.By("Successfully deleted all test scenarios and scale test resources")
 	})
 })
