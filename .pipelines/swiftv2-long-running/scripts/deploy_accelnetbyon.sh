e30a7cbf1134d331b400625c9460d94a4068a914
sivakami <sivakamis@microsoft.com>
2026-01-11 10:41:53 -0800

    Update tip sessions.

--- .pipelines/swiftv2-long-running/scripts/deploy_accelnetbyon.sh
+++ .pipelines/swiftv2-long-running/scripts/deploy_accelnetbyon.sh
@@ -10,14 +10,12 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/byon_helper.sh"
 
 cluster_names="aks-1 aks-2"
-tip_session_ids=(8fe6f0f9-1476-4c8c-8945-c16300e557a9 8fe6f0f9-1476-4c8c-8945-c16300e557a9 dc72458d-a95d-4913-9deb-e4f4d20ca4dd dc72458d-a95d-4913-9deb-e4f4d20ca4dd 744922f0-1651-409f-9475-18ae34602446 744922f0-1651-409f-9475-18ae34602446 f64e86da-c832-460b-995f-0e9fe2601d99 f64e86da-c832-460b-995f-0e9fe2601d99)
-
-# Define VMSS configurations: node_prefix, sku, nic_count, node_count
+tip_session_ids=(4aa2ca57-8dd0-4fbd-a961-bae4142dc33c 4aa2ca57-8dd0-4fbd-a961-bae4142dc33c 7356a39e-f8e5-40e0-815d-cd53f540d505 7356a39e-f8e5-40e0-815d-cd53f540d505 4e7a03fc-c541-4845-8805-73e45028f171 4e7a03fc-c541-4845-8805-73e45028f171 f64e86da-c832-460b-995f-0e9fe2601d99 f64e86da-c832-460b-995f-0e9fe2601d99)
 vmss_configs=(
-  "aclhigh1:Standard_D32s_v3:7"
-  "aclhigh2:Standard_D32s_v3:7"
-  "acldef1:Standard_D8s_v3:2"
-  "acldef2:Standard_D8s_v3:2"
+  "aclhigh1:Standard_D16s_v6:7"
+  "aclhigh2:Standard_D16s_v6:7"
+  "acldef1:Standard_D4s_v6:2"
+  "acldef2:Standard_D4s_v6:2"
 )
 
 create_l1vh_vmss() {
