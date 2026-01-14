3e8b05653faf45d8b49af9e1444257cca42776d2
paulyu <paulyu@microsoft.com>
2026-01-14 18:29:49 +0000

    format codes

--- cni/network/network_windows_test.go
+++ cni/network/network_windows_test.go
@@ -1423,7 +1423,6 @@ func TestSetupInfraVnetRoutingForMultitenancy(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			setupInfraVnetRoutingForMultitenancy(nil, nil, tt.epInfo)
 
