31716c550e92dd302cba3a28512468acec811407
Aranta Rokade <arantarokade@microsoft.com>
2026-01-22 20:40:48 +0000

    make linter exception inline

--- crd/multitenancy/api/v1alpha1/podnetwork.go
+++ crd/multitenancy/api/v1alpha1/podnetwork.go
@@ -20,6 +20,7 @@ import (
 // +kubebuilder:printcolumn:name="Subnet",type=string,priority=1,JSONPath=`.spec.subnetResourceID`
 // +kubebuilder:printcolumn:name="SubnetGUID",type=string,priority=1,JSONPath=`.spec.subnetGUID`
 // +kubebuilder:printcolumn:name="DeviceType",type=string,priority=1,JSONPath=`.spec.deviceType`
+//nolint:lll
 // +kubebuilder:validation:XValidation:rule="!has(oldSelf.status) || !has(oldSelf.status.status) || !(oldSelf.status.status in ['Ready','InUse']) || self.spec == oldSelf.spec",message="Spec is immutable when status is Ready or InUse."
 type PodNetwork struct {
 	metav1.TypeMeta   `json:",inline"`
