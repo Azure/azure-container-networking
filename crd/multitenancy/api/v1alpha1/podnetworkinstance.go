e115450a3f70cd15322ce5ac897650350b8d2d54
Aranta Rokade <arantarokade@microsoft.com>
2026-01-22 02:31:00 +0000

    add PN validation update check on ipConstraint

--- crd/multitenancy/api/v1alpha1/podnetworkinstance.go
+++ crd/multitenancy/api/v1alpha1/podnetworkinstance.go
@@ -36,7 +36,7 @@ type PodNetworkInstanceList struct {
 }
 
 // PodNetworkConfig describes a template for how to attach a PodNetwork to a Pod
-// +kubebuilder:validation:XValidation:rule="self.ipConstraint == '' || self.podIPReservationSize == 1",message="ipConstraint is only allowed when podIPReservationSize is 1"
+// +kubebuilder:validation:XValidation:rule="!has(self.ipConstraint) || size(self.ipConstraint) == 0 || self.podIPReservationSize == 1",message="ipConstraint can only be specified when podIPReservationSize is 1"
 type PodNetworkConfig struct {
 	// PodNetwork is the name of a PodNetwork resource
 	PodNetwork string `json:"podNetwork"`
@@ -49,6 +49,7 @@ type PodNetworkConfig struct {
 	// Examples:
 	//   - IPv4 address: "192.168.0.1"
 	//   - IPv4 CIDR:    "192.168.0.1/32"
+	// +kubebuilder:default=""
 	// +kubebuilder:validation:Optional
 	// +kubebuilder:validation:MaxLength=18
 	// +kubebuilder:validation:Pattern=`^$|^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\/32)?$`
