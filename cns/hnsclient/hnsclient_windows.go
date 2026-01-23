3ff1257fd4738e9697034834c6aa2fe181701440
paulyu <paulyu@microsoft.com>
2026-01-23 19:29:13 +0000

    fix destinationprefix

--- cns/hnsclient/hnsclient_windows.go
+++ cns/hnsclient/hnsclient_windows.go
@@ -541,8 +541,7 @@ func configureHostNCApipaEndpoint(
 	// keep Apipa Endpoint gw as 169.254.128.1 to make sure NC to host connectivity work for both Linux and Windows containers
 	hcnRoute := hcn.Route{
 		NextHop:           hnsLoopbackAdapterIPAddress,
-		DestinationPrefix: "0.0.0.0/0",
-		Metric:            50,
+		DestinationPrefix: "169.254.128.0/17",
 	}
 
 	endpoint.Routes = append(endpoint.Routes, hcnRoute)
