c7f4a04ea0d2e1de4573e10b4443e49f2d07a67f
QxBytes <alexanderlew@microsoft.com>
2026-01-30 15:56:34 -0800

    remove fileContent

--- cns/service_windows_test.go
+++ cns/service_windows_test.go
@@ -6,13 +6,11 @@ import (
 
 // postProcessPEMCert encrypts the pem to base64 with dpapi on windows
 func postProcessPEMCert(pem []byte) ([]byte, error) {
-	var fileContent []byte
 	// On Windows, the TLS certificate retriever expects DPAPI-encrypted content
 	encrypted, encryptErr := dpapi.Encrypt(string(pem))
 	if encryptErr != nil {
 		return nil, encryptErr
 	}
-	fileContent = []byte(encrypted)
 
-	return fileContent, nil
+	return []byte(encrypted), nil
 }
