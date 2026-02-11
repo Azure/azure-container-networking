ac56f7a65f35c668ccceb00df84e7b27736b7324
shufang <shchen@microsoft.com>
2026-02-10 21:49:02 -0800

    Add AlwaysWriteNodeInfoCRD in config and cap the prefix of IPV6.

--- cns/service/main.go
+++ cns/service/main.go
@@ -1419,18 +1419,20 @@ func InitializeCRDState(ctx context.Context, z *zap.Logger, httpRestService cns.
 		return errors.Wrapf(err, "failed to get node %s", nodeName)
 	}
 
-	if nodeInfoErr := createOrUpdateNodeInfoCRD(ctx, kubeConfig, node); nodeInfoErr != nil {
-		return errors.Wrap(nodeInfoErr, "error creating or updating nodeinfo crd")
+	if cnsconfig.AlwaysWriteNodeInfoCRD {
+		if nodeInfoErr := createOrUpdateNodeInfoCRD(ctx, kubeConfig, node); nodeInfoErr != nil {
+			return errors.Wrap(nodeInfoErr, "error creating or updating nodeinfo crd")
+		}
 	}
 
-	// check the Node labels for Swift V2
-	// if _, ok := node.Labels[configuration.LabelNodeSwiftV2]; ok {
-	// 	cnsconfig.EnableSwiftV2 = true
-	// 	cnsconfig.WatchPods = true
-	// 	if nodeInfoErr := createOrUpdateNodeInfoCRD(ctx, kubeConfig, node); nodeInfoErr != nil {
-	// 		return errors.Wrap(nodeInfoErr, "error creating or updating nodeinfo crd")
-	// 	}
-	// }
+	//check the Node labels for Swift V2
+	if _, ok := node.Labels[configuration.LabelNodeSwiftV2]; ok {
+		cnsconfig.EnableSwiftV2 = true
+		cnsconfig.WatchPods = true
+		if nodeInfoErr := createOrUpdateNodeInfoCRD(ctx, kubeConfig, node); nodeInfoErr != nil {
+			return errors.Wrap(nodeInfoErr, "error creating or updating nodeinfo crd")
+		}
+	}
 
 	// populate the NodeInfo CRD for Swift V1 dualstack scenario when enabled via config
 	if cnsconfig.EnableSwiftV1DualStack {
