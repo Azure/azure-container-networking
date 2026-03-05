9a1c34046474023668010e364922a4a8f4114093
beegii khurelsukh <bkhurelsukh@microsoft.com>
2026-03-05 10:46:07 +0000

    feat: More lint errors fixed

--- cns/service/main.go
+++ cns/service/main.go
@@ -581,7 +581,7 @@ func main() {
 
 		// Use connection string only if sovereign cloud is enabled
 		if ts.EnableAIInSovereignCloud && cnsconfig.TelemetrySettings.AppInsightsConnectionString != "" {
-			logger.InitAIWithConnectionString(aiConfig, cnsconfig.TelemetrySettings.AppInsightsConnectionString, ts.DisableTrace, ts.DisableMetric, ts.DisableEvent)
+			logger.InitAIWithConnectionString(aiConfig, cnsconfig.TelemetrySettings.AppInsightsConnectionString, ts.DisableTrace, ts.DisableMetric, ts.DisableEvent) //nolint:staticcheck
 		} else if aiKey := cnsconfig.TelemetrySettings.AppInsightsInstrumentationKey; aiKey != "" {
 			logger.InitAIWithIKey(aiConfig, aiKey, ts.DisableTrace, ts.DisableMetric, ts.DisableEvent)
 		} else {
