92e9efa2f5c033ae61c65df7abcb6e6d8d9791a4
Esteban Capillo <estebanca@microsoft.com>
2026-03-17 14:09:00 -0500

    fix: moved mock to a non OS based location

--- network/transparent_vlan_endpointclient_linux_test.go
+++ network/transparent_vlan_endpointclient_linux_test.go
@@ -1263,29 +1263,6 @@ func TestAddDefaultArp(t *testing.T) {
 	}
 }
 
-// mockIPTablesClient is a mock for the ipTablesClient interface that tracks calls.
-type mockIPTablesClient struct {
-	insertCalls []iptablesCall
-}
-
-type iptablesCall struct {
-	version   string
-	tableName string
-	chainName string
-	match     string
-	target    string
-}
-
-func (c *mockIPTablesClient) InsertIptableRule(version, tableName, chainName, match, target string) error {
-	c.insertCalls = append(c.insertCalls, iptablesCall{version, tableName, chainName, match, target})
-	return nil
-}
-
-func (c *mockIPTablesClient) AppendIptableRule(_, _, _, _, _ string) error { return nil }
-func (c *mockIPTablesClient) DeleteIptableRule(_, _, _, _, _ string) error { return nil }
-func (c *mockIPTablesClient) CreateChain(_, _, _ string) error             { return nil }
-func (c *mockIPTablesClient) RunCmd(_, _ string) error                     { return nil }
-
 // mockNetlinkRuleClient stubs vishvananda/netlink RuleList/RuleAdd so tests
 // never touch real netlink sockets or require CAP_NET_ADMIN.
 type mockNetlinkRuleClient struct {
