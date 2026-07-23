0e5ed246fc81e8f8ae137621216a1a2249f979fd
Evan Baker <rbtr@users.noreply.github.com>
2026-07-23 14:10:23 -0500

    fix(test): retry transient state observations (#4553)
    * fix(test): retry transient state observations
    
    * fix: address validator retry lint
    
    * fix: honor validation retry cancellation
    
    * test: cover retry timer cancellation

--- test/validate/validate.go
+++ test/validate/validate.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"log"
+	"time"
 
 	acnk8s "github.com/Azure/azure-container-networking/test/internal/kubernetes"
 	"github.com/pkg/errors"
@@ -38,6 +39,8 @@ const (
 	privilegedNamespace      = "kube-system"
 	IPv4ExpectedIPCount      = 1
 	DualstackExpectedIPCount = 2
+	stateValidationAttempts  = 3
+	stateValidationInterval  = 2 * time.Second
 )
 
 type Validator struct {
@@ -131,43 +134,87 @@ func (v *Validator) validateIPs(ctx context.Context, stateFileIps stateFileIpsFu
 	}
 
 	for index := range nodes.Items {
-		// get the privileged pod
-		pod, err := acnk8s.GetPodsByNode(ctx, v.clientset, namespace, labelSelector, nodes.Items[index].Name)
-		if err != nil {
-			return errors.Wrapf(err, "failed to get privileged pod")
-		}
-		if len(pod.Items) == 0 {
-			return errors.Errorf("there are no privileged pods on node - %v", nodes.Items[index].Name)
-		}
-		podName := pod.Items[0].Name
-		// exec into the pod to get the state file
-		log.Printf("Executing command %s on pod %s, container %s", cmd, podName, containerName)
-		result, _, err := acnk8s.ExecCmdOnPod(ctx, v.clientset, namespace, podName, containerName, cmd, v.config, true)
-		if err != nil {
-			return errors.Wrapf(err, "failed to exec into privileged pod - %s", podName)
-		}
-		filePodIps, err := stateFileIps(result)
-		if err != nil {
-			return errors.Wrapf(err, "failed to get pod ips from state file on node %v", nodes.Items[index].Name)
-		}
-		if len(filePodIps) == 0 && v.restartCase {
-			log.Printf("No pods found on node %s", nodes.Items[index].Name)
-			continue
-		}
-		// get the pod ips
-		podIps := getPodIPsWithoutNodeIP(ctx, v.clientset, nodes.Items[index])
-		// include IPs from Cilium internal endpoints (reserved:ingress) that are not real K8s pods.
-		// These only exist when L7 policy is enabled, indicated by the acns-security-agent pod with cilium-envoy container.
-		if hasL7PolicyEnabled(ctx, v.clientset, nodes.Items[index].Name) {
-			podIps = append(podIps, getCiliumInternalEndpointIPs(ctx, v.clientset, v.config, nodes.Items[index].Name)...)
-		}
+		node := nodes.Items[index]
+		var comparisonErr error
+		_, converged, validationErr := runValidationAttempts(ctx, stateValidationAttempts, stateValidationInterval, func() (bool, error) {
+			pod, err := acnk8s.GetPodsByNode(ctx, v.clientset, namespace, labelSelector, node.Name)
+			if err != nil {
+				return false, errors.Wrap(err, "failed to get privileged pod")
+			}
+			if len(pod.Items) == 0 {
+				return false, errors.Errorf("there are no privileged pods on node - %v", node.Name)
+			}
+			podName := pod.Items[0].Name
+			log.Printf("Executing command %s on pod %s, container %s", cmd, podName, containerName)
+			result, _, err := acnk8s.ExecCmdOnPod(ctx, v.clientset, namespace, podName, containerName, cmd, v.config, true)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to exec into privileged pod - %s", podName)
+			}
+			filePodIps, err := stateFileIps(result)
+			if err != nil {
+				return false, errors.Wrapf(err, "failed to get pod ips from state file on node %v", node.Name)
+			}
+			if len(filePodIps) == 0 && v.restartCase {
+				log.Printf("No pods found on node %s", node.Name)
+				return true, nil
+			}
 
-		if err := compareIPs(filePodIps, podIps); err != nil {
-			return errors.Wrapf(err, "State file validation failed for %s on node %s", checkType, nodes.Items[index].Name)
+			podIps := getPodIPsWithoutNodeIP(ctx, v.clientset, node)
+			if hasL7PolicyEnabled(ctx, v.clientset, node.Name) {
+				podIps = append(podIps, getCiliumInternalEndpointIPs(ctx, v.clientset, v.config, node.Name)...)
+			}
+			comparisonErr = compareIPs(filePodIps, podIps)
+			return comparisonErr == nil, nil
+		})
+		if validationErr != nil {
+			return validationErr
+		}
+		if !converged {
+			return errors.Wrapf(comparisonErr, "State file validation failed for %s on node %s", checkType, node.Name)
 		}
 	}
 	log.Printf("State file validation for %s passed", checkType)
 	return nil
+}
+
+func runValidationAttempts(
+	ctx context.Context,
+	maxAttempts int,
+	interval time.Duration,
+	validate func() (bool, error),
+) (attempts int, converged bool, validationErr error) {
+	if maxAttempts < 1 {
+		return 0, false, errors.New("validation attempts must be positive")
+	}
+
+	var lastErr error
+	for attempts = 1; ; attempts++ {
+		converged, validationErr = validate()
+		if converged {
+			if validationErr != nil {
+				return attempts, false, validationErr
+			}
+			return attempts, true, nil
+		}
+		lastErr = validationErr
+		if attempts >= maxAttempts {
+			return attempts, false, lastErr
+		}
+		if err := ctx.Err(); err != nil {
+			return attempts, false, errors.Wrap(err, "waiting to retry state validation")
+		}
+		if interval <= 0 {
+			continue
+		}
+
+		timer := time.NewTimer(interval)
+		select {
+		case <-ctx.Done():
+			timer.Stop()
+			return attempts, false, errors.Wrap(ctx.Err(), "waiting to retry state validation")
+		case <-timer.C:
+		}
+	}
 }
 
 func validateNodeProperties(nodes *corev1.NodeList, labels map[string]string, expectedIPCount int) error {
