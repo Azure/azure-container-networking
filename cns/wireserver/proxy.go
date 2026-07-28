9f1b7700445006b50f25d4455245fa6bcc3d50cc
Saksham Mittal (from Dev Box) <sakmittal@microsoft.com>
2026-07-28 10:37:44 -0700

    initial put/delete NC and subnet join related changes for RNC via NMA

--- cns/wireserver/proxy.go
+++ cns/wireserver/proxy.go
@@ -12,6 +12,7 @@ import (
 
 const (
 	joinNetworkURLFmt = `http://%s/machine/plugins/?comp=nmagent&type=NetworkManagement/joinedVirtualNetworks/%s/api-version/1`
+	joinSubnetURLFmt  = `http://%s/machine/plugins/?comp=nmagent&type=NetworkManagement/joinedVirtualNetworks/%s/joinedSubnets/%s/authenticationToken/%s/api-version/1?useLegacyChannel=false`
 	publishNCURLFmt   = `http://%s/machine/plugins/?comp=nmagent&type=NetworkManagement/interfaces/%s/networkContainers/%s/authenticationToken/%s/api-version/1`
 	unpublishNCURLFmt = `http://%s/machine/plugins/?comp=nmagent&type=NetworkManagement/interfaces/%s/networkContainers/%s/authenticationToken/%s/api-version/1/method/DELETE`
 )
@@ -21,8 +22,14 @@ type Proxy struct {
 	HTTPClient do
 }
 
-func (p *Proxy) JoinNetwork(ctx context.Context, vnetID string) (*http.Response, error) {
-	reqURL := fmt.Sprintf(joinNetworkURLFmt, p.Host, vnetID)
+func (p *Proxy) JoinNetwork(ctx context.Context, vnetID string, useRNCPublisher bool) (*http.Response, error) {
+	var joinNetworkURLFormat string
+	if useRNCPublisher {
+		joinNetworkURLFormat = joinNetworkURLFmt + "?useLegacyChannel=false"
+	} else {
+		joinNetworkURLFormat = joinNetworkURLFmt
+	}
+	reqURL := fmt.Sprintf(joinNetworkURLFormat, p.Host, vnetID)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, bytes.NewBufferString(`""`))
 	if err != nil {
@@ -39,8 +46,32 @@ func (p *Proxy) JoinNetwork(ctx context.Context, vnetID string) (*http.Response,
 	return resp, nil
 }
 
-func (p *Proxy) PublishNC(ctx context.Context, ncParams cns.NetworkContainerParameters, payload []byte) (*http.Response, error) {
-	reqURL := fmt.Sprintf(publishNCURLFmt, p.Host, ncParams.AssociatedInterfaceID, ncParams.NCID, ncParams.AuthToken)
+func (p *Proxy) JoinSubnet(ctx context.Context, vnetID, subnetName string, ncParams cns.NetworkContainerParameters) (*http.Response, error) {
+	reqURL := fmt.Sprintf(joinSubnetURLFmt, p.Host, vnetID, subnetName, ncParams.AuthToken)
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, bytes.NewBufferString(`""`))
+	if err != nil {
+		return nil, errors.Wrap(err, "wireserver proxy: join subnet: could not build http request")
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := p.HTTPClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "wireserver proxy: join subnet: could not perform http request")
+	}
+
+	return resp, nil
+}
+
+func (p *Proxy) PublishNC(ctx context.Context, ncParams cns.NetworkContainerParameters, payload []byte, useRNCPublisher bool) (*http.Response, error) {
+	var publishNCURLFormat string
+	if useRNCPublisher {
+		publishNCURLFormat = publishNCURLFmt + "?useLegacyChannel=false"
+	} else {
+		publishNCURLFormat = publishNCURLFmt
+	}
+	reqURL := fmt.Sprintf(publishNCURLFormat, p.Host, ncParams.AssociatedInterfaceID, ncParams.NCID, ncParams.AuthToken)
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, reqURL, bytes.NewBuffer(payload))
 	if err != nil {
@@ -57,8 +88,14 @@ func (p *Proxy) PublishNC(ctx context.Context, ncParams cns.NetworkContainerPara
 	return resp, nil
 }
 
-func (p *Proxy) UnpublishNC(ctx context.Context, ncParams cns.NetworkContainerParameters, payload []byte) (*http.Response, error) {
-	reqURL := fmt.Sprintf(unpublishNCURLFmt, p.Host, ncParams.AssociatedInterfaceID, ncParams.NCID, ncParams.AuthToken)
+func (p *Proxy) UnpublishNC(ctx context.Context, ncParams cns.NetworkContainerParameters, payload []byte, useRNCPublisher bool) (*http.Response, error) {
+	var unpublishNCURLFormat string
+	if useRNCPublisher {
+		unpublishNCURLFormat = unpublishNCURLFmt + "?useLegacyChannel=false"
+	} else {
+		unpublishNCURLFormat = unpublishNCURLFmt
+	}
+	reqURL := fmt.Sprintf(unpublishNCURLFormat, p.Host, ncParams.AssociatedInterfaceID, ncParams.NCID, ncParams.AuthToken)
 
 	// a POST to wireserver must contain a body. For legacy purposes,
 	// an empty json string (two quote characters) should be sent by default.
