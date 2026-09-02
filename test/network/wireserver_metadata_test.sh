310f4e810754831c9219bc077e8857c83a547f67
Alexander <39818795+QxBytes@users.noreply.github.com>
2026-09-02 16:22:53 +0000

    ci: add random for uniqueness across retries

--- test/network/wireserver_metadata_test.sh
+++ test/network/wireserver_metadata_test.sh
@@ -11,13 +11,15 @@ if [ -n "$POOL" ]; then
     OVERRIDES=(--overrides="{\"spec\":{\"nodeSelector\":{\"agentpool\":\"${POOL}\"}}}")
 fi
 
-kubectl run "$POD_NAME-wireserver" -it --rm --image busybox --restart Never "${OVERRIDES[@]}" -- wget --timeout=3 --header=Metadata:true "http://168.63.129.16/machine/plugins?comp=nmagent&type=getinterfaceinfov1"
+SUFFIX="${RANDOM}"
+
+kubectl run "$POD_NAME-wireserver-${SUFFIX}" -it --rm --image busybox --restart Never "${OVERRIDES[@]}" -- wget --timeout=3 --header=Metadata:true "http://168.63.129.16/machine/plugins?comp=nmagent&type=getinterfaceinfov1"
 if [ $? -eq 0 ]; then
     echo "wireserver connectivity expected to fail but succeeded"
     exit 1
 fi
 
-kubectl run "$POD_NAME-metadata" -it --rm --image busybox --restart Never "${OVERRIDES[@]}" -- wget --timeout=3 --header=Metadata:true "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
+kubectl run "$POD_NAME-metadata-${SUFFIX}" -it --rm --image busybox --restart Never "${OVERRIDES[@]}" -- wget --timeout=3 --header=Metadata:true "http://169.254.169.254/metadata/instance?api-version=2021-02-01"
 if [ $? -ne 0 ]; then
     echo "metadata server connectivity expected to succeed but failed"
     exit 1
